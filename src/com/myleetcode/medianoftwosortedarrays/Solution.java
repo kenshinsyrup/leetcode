2f42ad17efa104110e4425c18fa76b6228dd8ffa
Kenshinsyrup <kenshinsyrup@gmail.com>
2018-11-07 11:47:36 -0800

    fix variable symbol

--- src/com/myleetcode/medianoftwosortedarrays/Solution.java
+++ src/com/myleetcode/medianoftwosortedarrays/Solution.java
@@ -13,12 +13,12 @@ public class Solution {
      */
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         // 此题目要求的解：1、两个数组合并后的中位数；2、Big-O log(m+n)
-        int m = A.length;
-        int n = B.length;
+        int m = nums1.length;
+        int n = nums2.length;
         if (m > n) { // to ensure m<=n
-            int[] temp = A;
-            A = B;
-            B = temp;
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
             int tmp = m;
             m = n;
             n = tmp;
@@ -27,18 +27,18 @@ public class Solution {
         while (iMin <= iMax) {
             int i = (iMin + iMax) / 2;
             int j = halfLen - i;
-            if (i < iMax && B[j - 1] > A[i]) {
+            if (i < iMax && nums2[j - 1] > nums1[i]) {
                 iMin = i + 1; // i is too small
-            } else if (i > iMin && A[i - 1] > B[j]) {
+            } else if (i > iMin && nums1[i - 1] > nums2[j]) {
                 iMax = i - 1; // i is too big
             } else { // i is perfect
                 int maxLeft = 0;
                 if (i == 0) {
-                    maxLeft = B[j - 1];
+                    maxLeft = nums2[j - 1];
                 } else if (j == 0) {
-                    maxLeft = A[i - 1];
+                    maxLeft = nums1[i - 1];
                 } else {
-                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
                 }
                 if ((m + n) % 2 == 1) {
                     return maxLeft;
@@ -46,11 +46,11 @@ public class Solution {
 
                 int minRight = 0;
                 if (i == m) {
-                    minRight = B[j];
+                    minRight = nums2[j];
                 } else if (j == n) {
-                    minRight = A[i];
+                    minRight = nums1[i];
                 } else {
-                    minRight = Math.min(B[j], A[i]);
+                    minRight = Math.min(nums2[j], nums1[i]);
                 }
 
                 return (maxLeft + minRight) / 2.0;
