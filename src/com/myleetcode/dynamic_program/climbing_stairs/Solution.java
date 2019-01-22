042a0021e74b84b1ddf84ad73a5f1cb32e0e9eea
Kenshinsyrup <kenshinsyrup@gmail.com>
2019-01-22 14:58:59 -0800

    add some links about dp to dynamic_program/climbing_stairs/Solution.java

--- src/com/myleetcode/dynamic_program/climbing_stairs/Solution.java
+++ src/com/myleetcode/dynamic_program/climbing_stairs/Solution.java
@@ -13,6 +13,10 @@ class Solution {
         if(n < 3){
             return n;
         }
+
+        // 关于dp的一个讲解
+        // https://www.quora.com/Are-there-any-good-resources-or-tutorials-for-dynamic-programming-DP-besides-the-TopCoder-tutorial/answer/Michal-Danil%C3%A1k?share=1&srid=3OBi
+        // https://www.hackerearth.com/zh/practice/algorithms/dynamic-programming/introduction-to-dynamic-programming-1/tutorial/
         
 //         这个号称是经典的dp问题
 //         爬台阶，每次都只能选择爬1还是2级，问多少种方式到底第n级。也就是从1到n一共n个台阶。
