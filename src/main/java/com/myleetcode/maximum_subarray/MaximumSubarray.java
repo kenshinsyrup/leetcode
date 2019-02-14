9771f14e29ac440d42e292af5aa15f4952545c51
Kenshinsyrup <kenshinsyrup@gmail.com>
2019-02-14 14:33:43 -0800

    53. Maximum Subarray

---
+++ src/main/java/com/myleetcode/maximum_subarray/MaximumSubarray.java
@@ -0,0 +1,102 @@
+package com.myleetcode.maximum_subarray;
+
+public class MaximumSubarray {
+    /**
+     * 53. Maximum Subarray
+     * Easy
+     *
+     * 3549
+     *
+     * 124
+     *
+     * Favorite
+     *
+     * Share
+     * Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+     *
+     * Example:
+     *
+     * Input: [-2,1,-3,4,-1,2,1,-5,4],
+     * Output: 6
+     * Explanation: [4,-1,2,1] has the largest sum = 6.
+     * Follow up:
+     *
+     * If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+     */
+
+    /**
+     * Dynamic Programming
+     * Divide and Conquer
+     * Array
+     */
+
+    /**
+     * Apple
+     * |
+     * 17
+     *
+     * Microsoft
+     * |
+     * 14
+     *
+     * Facebook
+     * |
+     * 11
+     *
+     * Google
+     * |
+     * 10
+     *
+     * Amazon
+     * |
+     * 8
+     *
+     * LinkedIn
+     * |
+     * 7
+     *
+     * Two Sigma
+     * |
+     * 7
+     *
+     * Alibaba
+     * |
+     * 4
+     *
+     * JPMorgan
+     * |
+     * 4
+     *
+     * Bloomberg
+     * |
+     * 3
+     *
+     * Uber
+     * |
+     * 3
+     *
+     * Capital One
+     * |
+     * 3
+     *
+     * Adobe
+     * |
+     * 2
+     *
+     * Baidu
+     * |
+     * 2
+     *
+     * Goldman Sachs
+     * |
+     * 2
+     *
+     * Nvidia
+     * |
+     * 2
+     *
+     * Zillow
+     * |
+     * 2
+     */
+}
