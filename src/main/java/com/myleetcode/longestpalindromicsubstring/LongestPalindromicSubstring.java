ff4038408b9ceca479217ea614736966f8e7add7
Kenshinsyrup <kenshinsyrup@gmail.com>
2019-02-14 15:20:03 -0800

    5. Longest Palindromic Substring

--- src/main/java/com/myleetcode/longestpalindromicsubstring/LongestPalindromicSubstring.java
+++ src/main/java/com/myleetcode/longestpalindromicsubstring/LongestPalindromicSubstring.java
@@ -1,17 +1,99 @@
 package com.myleetcode.longestpalindromicsubstring;
-/*
-5. Longest Palindromic Substring
-Medium
 
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+class LongestPalindromicSubstring{
+    /**
+     * 5. Longest Palindromic Substring
+     * Medium
+     *
+     * 2942
+     *
+     * 289
+     *
+     * Favorite
+     *
+     * Share
+     * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+     *
+     * Example 1:
+     *
+     * Input: "babad"
+     * Output: "bab"
+     * Note: "aba" is also a valid answer.
+     * Example 2:
+     *
+     * Input: "cbbd"
+     * Output: "bb"
+     */
 
-Example 1:
+    /**
+     * Dynamic Programming
+     * String
+     */
 
-Input: "babad"
-Output: "bab"
-Note: "aba" is also a valid answer.
-Example 2:
+    /**
+     * Amazon
+     * |
+     * 40
+     *
+     * Microsoft
+     * |
+     * 13
+     *
+     * Facebook
+     * |
+     * 11
+     *
+     * Google
+     * |
+     * 10
+     *
+     * Adobe
+     * |
+     * 7
+     *
+     * Apple
+     * |
+     * 6
+     *
+     * Yahoo
+     * |
+     * 4
+     *
+     * Bloomberg
+     * |
+     * 3
+     *
+     * Airbnb
+     * |
+     * 3
+     *
+     * Roblox
+     * |
+     * 3
+     *
+     * Baidu
+     * |
+     * 2
+     *
+     * Cisco
+     * |
+     * 2
+     *
+     * NetEase
+     * |
+     * 2
+     *
+     * Pure Storage
+     * |
+     * 2
+     *
+     * GoDaddy
+     * |
+     * 2
+     *
+     * JPMorgan
+     * |
+     * 2
+     */
+}
 
-Input: "cbbd"
-Output: "bb"
-*/
