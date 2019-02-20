f827e9afb3687338c4f74d73464b66b0d1040832
Kenshin <hi_appler@126.com>
2019-02-20 15:10:14 -0800

    131. Palindrome Partitioning

---
+++ src/main/java/com/myleetcode/palindrome_partitioning/PalindromePartitioning.java
@@ -0,0 +1,41 @@
+package com.myleetcode.palindrome_partitioning;
+
+public class PalindromePartitioning {
+    /**
+     * 131. Palindrome Partitioning
+     * Medium
+     *
+     * 761
+     *
+     * 29
+     *
+     * Favorite
+     *
+     * Share
+     * Given a string s, partition s such that every substring of the partition is a palindrome.
+     *
+     * Return all possible palindrome partitioning of s.
+     *
+     * Example:
+     *
+     * Input: "aab"
+     * Output:
+     * [
+     *   ["aa","b"],
+     *   ["a","a","b"]
+     * ]
+     */
+
+    /**
+     * Backtracking
+     *
+     */
+
+    /**
+     * 0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+     *
+     * Amazon
+     * |
+     * 2
+     */
+}
---
+++ src/main/java/com/myleetcode/palindrome_partitioning/Solution.java
@@ -0,0 +1,55 @@
+package com.myleetcode.palindrome_partitioning;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<List<String>> partition(String s) {
+//        https://leetcode.com/problems/subsets/discuss/27281/A-general-approach-to-backtracking-questions-in-Java-(Subsets-Permutations-Combination-Sum-Palindrome-Partitioning)
+
+        List<List<String>> ans = new ArrayList<List<String>>();
+
+        if (s == null) {
+            return ans;
+        }
+
+        List<String> temp = new ArrayList<String>();
+
+        partitionByBacktracking(s, ans, temp, 0);
+
+        return ans;
+    }
+
+    private void partitionByBacktracking(String s, List<List<String>> ans, List<String> temp, int start) {
+        if (start >= s.length()) {
+            ans.add(new ArrayList(temp));
+        } else {
+            for (int i = start; i < s.length(); i++) {
+                // add palindrome
+                String substr = s.substring(start, i + 1);
+                if (isPalindrome(substr)) {
+                    temp.add(substr);
+
+                    partitionByBacktracking(s, ans, temp, i + 1);
+
+                    temp.remove(temp.size() - 1);
+                }
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
