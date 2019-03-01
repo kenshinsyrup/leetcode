5a689c14e104f834615c9b4388708d23a72d26b6
Kenshin <hi_appler@126.com>
2019-03-01 12:25:24 -0800

    56. Merge Intervals

---
+++ src/main/java/com/myleetcode/array/merge_intervals/MergeIntervals.java
@@ -0,0 +1,115 @@
+package com.myleetcode.array.merge_intervals;
+
+public class MergeIntervals {
+    /**
+     * 56. Merge Intervals
+     * Medium
+     *
+     * 1727
+     *
+     * 131
+     *
+     * Favorite
+     *
+     * Share
+     * Given a collection of intervals, merge all overlapping intervals.
+     *
+     * Example 1:
+     *
+     * Input: [[1,3],[2,6],[8,10],[15,18]]
+     * Output: [[1,6],[8,10],[15,18]]
+     * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+     * Example 2:
+     *
+     * Input: [[1,4],[4,5]]
+     * Output: [[1,5]]
+     * Explanation: Intervals [1,4] and [4,5] are considered overlapping.
+     */
+
+    /**
+     * Array
+     * Sort
+     */
+
+    /**
+     * Facebook
+     * |
+     * 32
+     *
+     * Palantir Technologies
+     * |
+     * 27
+     *
+     * Google
+     * |
+     * 26
+     *
+     * Uber
+     * |
+     * 16
+     *
+     * Bloomberg
+     * |
+     * 15
+     *
+     * Amazon
+     * |
+     * 11
+     *
+     * Microsoft
+     * |
+     * 8
+     *
+     * Apple
+     * |
+     * 7
+     *
+     * Salesforce
+     * |
+     * 6
+     *
+     * Oracle
+     * |
+     * 5
+     *
+     * Cisco
+     * |
+     * 4
+     *
+     * Goldman Sachs
+     * |
+     * 4
+     *
+     * Twitter
+     * |
+     * 4
+     *
+     * LinkedIn
+     * |
+     * 3
+     *
+     * Yelp
+     * |
+     * 3
+     *
+     * Snapchat
+     * |
+     * 3
+     *
+     * Dataminr
+     * |
+     * 3
+     *
+     * Paypal
+     * |
+     * 2
+     *
+     * Adobe
+     * |
+     * 2
+     *
+     * Intuit
+     * |
+     * 2
+     */
+}
