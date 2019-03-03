7f8970fd78ce9379794562706854c8583c0b50a2
Kenshin <hi_appler@126.com>
2019-03-02 22:14:16 -0800

    253. Meeting Rooms II

---
+++ src/main/java/com/myleetcode/array/meeting_rooms_ii/MeetingRoomsII.java
@@ -0,0 +1,75 @@
+package com.myleetcode.array.meeting_rooms_ii;
+
+public class MeetingRoomsII {
+    /**
+     * 253. Meeting Rooms II
+     * Medium
+     *
+     * 1108
+     *
+     * 20
+     *
+     * Favorite
+     *
+     * Share
+     * Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
+     *
+     * Example 1:
+     *
+     * Input: [[0, 30],[5, 10],[15, 20]]
+     * Output: 2
+     * Example 2:
+     *
+     * Input: [[7,10],[2,4]]
+     * Output: 1
+     */
+
+    /**
+     * Heap
+     * Greedy
+     * Sort
+     */
+
+    /**
+     * Facebook
+     * |
+     * 65
+     *
+     * Google
+     * |
+     * 33
+     *
+     * Amazon
+     * |
+     * 15
+     *
+     * Microsoft
+     * |
+     * 14
+     *
+     * Bloomberg
+     * |
+     * 8
+     *
+     * Uber
+     * |
+     * 6
+     *
+     * Yelp
+     * |
+     * 3
+     *
+     * Apple
+     * |
+     * 3
+     *
+     * Snapchat
+     * |
+     * 2
+     *
+     * Cisco
+     * |
+     * 2
+     */
+
+}
