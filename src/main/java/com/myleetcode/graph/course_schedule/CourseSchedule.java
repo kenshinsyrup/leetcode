313f9f0a54adc55c516dc8ec25af8876a25bf962
Kenshin <hi_appler@126.com>
2019-03-05 20:14:34 -0800

    207. Course Schedule

---
+++ src/main/java/com/myleetcode/graph/course_schedule/CourseSchedule.java
@@ -0,0 +1,88 @@
+package com.myleetcode.graph.course_schedule;
+
+public class CourseSchedule {
+    /**
+     * 207. Course Schedule
+     * Medium
+     *
+     * 1461
+     *
+     * 76
+     *
+     * Favorite
+     *
+     * Share
+     * There are a total of n courses you have to take, labeled from 0 to n-1.
+     *
+     * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+     *
+     * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+     *
+     * Example 1:
+     *
+     * Input: 2, [[1,0]]
+     * Output: true
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0. So it is possible.
+     * Example 2:
+     *
+     * Input: 2, [[1,0],[0,1]]
+     * Output: false
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0, and to take course 0 you should
+     *              also have finished course 1. So it is impossible.
+     * Note:
+     *
+     * The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+     * You may assume that there are no duplicate edges in the input prerequisites.
+     */
+
+    /**
+     * DFS: Depth-first Search
+     * BFS: Breadth-first Search
+     * Graph
+     * Topological Sort
+     */
+
+    /**
+     * Amazon
+     * |
+     * 10
+     *
+     * Facebook
+     * |
+     * 7
+     *
+     * Google
+     * |
+     * 5
+     *
+     * Uber
+     * |
+     * 3
+     *
+     * Intuit
+     * |
+     * 3
+     *
+     * Goldman Sachs
+     * |
+     * 3
+     *
+     * Salesforce
+     * |
+     * 2
+     *
+     * Paypal
+     * |
+     * 2
+     *
+     * Nutanix
+     * |
+     * 2
+     *
+     * eBay
+     * |
+     * 2
+     */
+}
