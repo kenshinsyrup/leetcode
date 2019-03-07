396f77e5ad90d4899fe3c9860a6144492a9c0b83
Kenshin <hi_appler@126.com>
2019-03-06 16:05:21 -0800

    175. Combine Two Tables

---
+++ src/main/java/com/myleetcode/database/combine_two_tables/Solution.java
@@ -0,0 +1,13 @@
+package com.myleetcode.database.combine_two_tables;
+
+public class Solution {
+    /**
+     * # Write your MySQL query statement below
+     *
+     * # check this https://www.codeproject.com/Articles/33052/Visual-Representation-of-SQL-Joins
+     * select FirstName, LastName, City, State
+     * from Person left join Address
+     * on Person.PersonId = Address.PersonId;
+     *
+     */
+}
