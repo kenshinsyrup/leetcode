0008277a1188d6625f2c5e6cd6d493263189d198
Kenshin <hi_appler@126.com>
2019-03-13 16:40:48 -0700

    445. Add Two Numbers II

---
+++ src/main/java/com/myleetcode/linked_list/add_two_numbers_ii/AddTwoNumbersII.java
@@ -0,0 +1,49 @@
+package com.myleetcode.linked_list.add_two_numbers_ii;
+
+public class AddTwoNumbersII {
+    /**
+     * 445. Add Two Numbers II
+     * Medium
+     *
+     * 643
+     *
+     * 87
+     *
+     * Favorite
+     *
+     * Share
+     * You are given two non-empty linked lists representing two non-negative integers. The most significant digit comes first and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+     *
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     *
+     * Follow up:
+     * What if you cannot modify the input lists? In other words, reversing the lists is not allowed.
+     *
+     * Example:
+     *
+     * Input: (7 -> 2 -> 4 -> 3) + (5 -> 6 -> 4)
+     * Output: 7 -> 8 -> 0 -> 7
+     */
+
+    /**
+     * Linked List
+     */
+
+    /**
+     * Microsoft
+     * |
+     * 11
+     *
+     * Bloomberg
+     * |
+     * 8
+     *
+     * Amazon
+     * |
+     * 5
+     *
+     * Facebook
+     * |
+     * 5
+     */
+}
---
+++ src/main/java/com/myleetcode/linked_list/add_two_numbers_ii/Solution.java
@@ -0,0 +1,72 @@
+package com.myleetcode.linked_list.add_two_numbers_ii;
+
+import com.myleetcode.utils.list_node.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        // 看起来和2. Add Two Numbers很像。反转l1和l2之后，就一样了
+        // https://leetcode.com/problems/add-two-numbers-ii/discuss/92640/Java-Solution-by-Reversing-LinkedList-beating-96
+
+        return addTwoNumbersByReverseAndAdd(l1, l2);
+    }
+
+    private ListNode addTwoNumbersByReverseAndAdd(ListNode l1, ListNode l2){
+        // reverse linked list
+        l1 = reverseLinkedList(l1);
+        l2 = reverseLinkedList(l2);
+
+        ListNode retDummyHeadPre = new ListNode(-1);
+        ListNode retHeadPre = retDummyHeadPre;
+        int sum = 0;
+        int carry = 0;
+        while(l1 != null || l2 != null || carry != 0){
+            sum = 0;
+            if(l1 != null && l2 != null){
+                sum += l1.val + l2.val + carry;
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }else if(l1 != null){
+                sum += l1.val + carry;
+
+                l1 = l1.next;
+            }else if(l2 != null){
+                sum += l2.val + carry;
+
+                l2 = l2.next;
+            }else{
+                sum += carry;
+            }
+
+            carry = sum / 10;
+            retDummyHeadPre.next = new ListNode(sum % 10);
+
+            retDummyHeadPre = retDummyHeadPre.next;
+        }
+
+        return reverseLinkedList(retHeadPre.next);
+    }
+
+    // 请熟练掌握反转链表的写法
+    private ListNode reverseLinkedList(ListNode lHead){
+        ListNode tempNode = new ListNode(-1); // keep lHead's next node
+        ListNode pre = null; // keep lHead's pre node, we will return this at last, because this is the head of the reversed list. At that time, lHead will be null
+        while(lHead != null){
+            tempNode = lHead.next;
+            lHead.next = pre;
+
+            pre = lHead;
+            lHead = tempNode;
+        }
+
+        return pre;
+    }
+}
