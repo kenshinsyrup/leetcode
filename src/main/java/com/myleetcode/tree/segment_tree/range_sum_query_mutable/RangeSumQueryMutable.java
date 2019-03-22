c12ca4e7029b439af1d40887bd782ad1959ba5ac
Kenshin <hi_appler@126.com>
2019-03-22 14:22:27 -0700

    307. Range Sum Query - Mutable

---
+++ src/main/java/com/myleetcode/tree/segment_tree/range_sum_query_mutable/RangeSumQueryMutable.java
@@ -0,0 +1,42 @@
+package com.myleetcode.tree.segment_tree.range_sum_query_mutable;
+
+public class RangeSumQueryMutable {
+    /**
+     * 307. Range Sum Query - Mutable
+     * Medium
+     *
+     * 630
+     *
+     * 53
+     *
+     * Favorite
+     *
+     * Share
+     * Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+     *
+     * The update(i, val) function modifies nums by updating the element at index i to val.
+     *
+     * Example:
+     *
+     * Given nums = [1, 3, 5]
+     *
+     * sumRange(0, 2) -> 9
+     * update(1, 2)
+     * sumRange(0, 2) -> 8
+     * Note:
+     *
+     * The array is only modifiable by the update function.
+     * You may assume the number of calls to update and sumRange function is distributed evenly.
+     */
+
+    /**
+     * Segment Tree
+     * Binary Indexed Tree
+     */
+
+    /**
+     * Google
+     * |
+     * 2
+     */
+}
