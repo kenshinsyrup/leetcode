f482803acd44a795838531a02f13b45c0cb915ec
Kenshin <hi_appler@126.com>
2019-04-01 15:07:31 -0700

    31. Next Permutation

---
+++ src/main/java/com/myleetcode/math/next_permutation/NextPermutation.java
@@ -0,0 +1,74 @@
+package com.myleetcode.math.next_permutation;
+
+public class NextPermutation {
+    /**
+     * 31. Next Permutation
+     * Medium
+     *
+     * 1583
+     *
+     * 484
+     *
+     * Favorite
+     *
+     * Share
+     * Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+     *
+     * If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+     *
+     * The replacement must be in-place and use only constant extra memory.
+     *
+     * Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+     *
+     * 1,2,3 → 1,3,2
+     * 3,2,1 → 1,2,3
+     * 1,1,5 → 1,5,1
+     */
+
+    /**
+     * Array
+     * Math
+     */
+
+    /**
+     * Facebook
+     * |
+     * 29
+     *
+     * Google
+     * |
+     * 13
+     *
+     * Sumologic
+     * |
+     * 9
+     *
+     * Microsoft
+     * |
+     * 7
+     *
+     * Amazon
+     * |
+     * 3
+     *
+     * Adobe
+     * |
+     * 3
+     *
+     * Uber
+     * |
+     * 3
+     *
+     * Quora
+     * |
+     * 3
+     *
+     * Bloomberg
+     * |
+     * 3
+     *
+     * Houzz
+     * |
+     * 2
+     */
+}
