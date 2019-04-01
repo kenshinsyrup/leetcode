8bf54bb762f593d8f395101e394495ec1b2c76cf
Kenshin <hi_appler@126.com>
2019-03-31 17:27:41 -0700

    19. Remove Nth Node From End of List

---
+++ src/main/java/com/myleetcode/linked_list/remove_nth_node_from_end_of_list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,69 @@
+package com.myleetcode.linked_list.remove_nth_node_from_end_of_list;
+
+public class RemoveNthNodeFromEndOfList {
+    /**
+     * 19. Remove Nth Node From End of List
+     * Medium
+     *
+     * 1607
+     *
+     * 124
+     *
+     * Favorite
+     *
+     * Share
+     * Given a linked list, remove the n-th node from the end of list and return its head.
+     *
+     * Example:
+     *
+     * Given linked list: 1->2->3->4->5, and n = 2.
+     *
+     * After removing the second node from the end, the linked list becomes 1->2->3->5.
+     * Note:
+     *
+     * Given n will always be valid.
+     *
+     * Follow up:
+     *
+     * Could you do this in one pass?
+     */
+
+    /**
+     * Linked List
+     * Two Pointers
+     */
+
+    /**
+     * Google
+     * |
+     * 9
+     *
+     * Microsoft
+     * |
+     * 6
+     *
+     * Amazon
+     * |
+     * 6
+     *
+     * Adobe
+     * |
+     * 4
+     *
+     * Apple
+     * |
+     * 2
+     *
+     * Bloomberg
+     * |
+     * 2
+     *
+     * Uber
+     * |
+     * 2
+     *
+     * Visa
+     * |
+     * 2
+     */
+}
---
+++ src/main/java/com/myleetcode/linked_list/remove_nth_node_from_end_of_list/Solution.java
@@ -0,0 +1,87 @@
+package com.myleetcode.linked_list.remove_nth_node_from_end_of_list;
+
+import com.myleetcode.utils.list_node.ListNode;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // return removeNthFromEndByTwoPass(head, n);
+        return removeNthFromEndByOnePass(head, n);
+    }
+
+    // TC: O(N)
+    // SC: O(1)
+    // follow up: one pass
+    // we need two pointers, slow and fast. if we keep slow and fast have a gap of n nodes, then if fast points to the end of linkedlist, slow must points to the n node from the node.
+    // for the case that the n node from the end is the head, we need add a dummy node before the head.
+    // and, because we need to delete the n node from the end, in linkedlist, we must has its pre node, so we need to find the n+1 node from the end, so in the solution, we want to find the n+1 node from the end, ie the gap is n+1
+    private ListNode removeNthFromEndByOnePass(ListNode head, int n){
+        // special case
+        if(n < 0 || head == null){
+            return head;
+        }
+
+        ListNode dummyPreHead = new ListNode(-1);
+        dummyPreHead.next = head;
+
+        ListNode fastPointer = dummyPreHead;
+        ListNode slowPointer = dummyPreHead;
+        // move fastPointer first to create the gap
+        for(int i = 1; i <= n+1; i++){
+            fastPointer = fastPointer.next;
+        }
+
+        // fastPointer moves to the end, slowPointer follows to keep the gap
+        while(fastPointer != null){
+            fastPointer = fastPointer.next;
+            slowPointer = slowPointer.next;
+        }
+
+        // delete
+        slowPointer.next = slowPointer.next.next;
+
+        return dummyPreHead.next;
+
+    }
+
+
+    // TC: O(N), N is the length of linkedlist
+    // SC: O(1), no extra space need
+    // intuition: remove the (len-n)th node, so we could first traverse the linked list to get its length, then traverse to remove the specific node
+    private ListNode removeNthFromEndByTwoPass(ListNode head, int n){
+        // special case
+        if(n < 0 || head == null){
+            return head;
+        }
+
+        // get linkedlist length
+        ListNode dummyHead = head;
+        int len = 0;
+        while(dummyHead != null){
+            dummyHead = dummyHead.next;
+            len++;
+        }
+
+        // remove the (len-n)th node, we find the pre node ie the (len-n-1)th node, then easily delete the (len-n)th node.
+        // so first we add ad preHead node to linkedlist, then we traverse to find the pre should be deleted node
+        ListNode dummyPreHead = new ListNode(-1);
+        dummyPreHead.next = head; // keep the reference to head. and add a pre node for head to let the function work.
+        ListNode dummyPre = dummyPreHead;// use for traverse to find the pre delete node
+        int preIdx = len - n - 1;
+        while(preIdx >= 0){
+            dummyPre = dummyPre.next;
+            preIdx--;
+        }
+
+        dummyPre.next = dummyPre.next.next; // delete the specific node
+
+        return dummyPreHead.next;//return "head"
+    }
+}
