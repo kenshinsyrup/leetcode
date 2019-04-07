00d3d62d74698220432d87af7eb7b49b87e55b21
Kenshin <hi_appler@126.com>
2019-04-07 15:37:44 -0700

    optimize: 255. Verify Preorder Sequence in Binary Search Tree

--- src/main/java/com/myleetcode/tree/verify_preorder_sequence_in_binary_search_tree/Solution.java
+++ src/main/java/com/myleetcode/tree/verify_preorder_sequence_in_binary_search_tree/Solution.java
@@ -1,13 +1,108 @@
 package com.myleetcode.tree.verify_preorder_sequence_in_binary_search_tree;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 
 class Solution {
     public boolean verifyPreorder(int[] preorder) {
         // return verifyPreorderByDFS(preorder);// Wrong
 
-        return verifyPreorderByDFS(preorder);
+        // return verifyPreorderByDFS(preorder);
+
+        // return verifyPreorderByTwoStack(preorder);
+        return verifyPreorderByOneStack(preorder);
+    }
+
+    // TC: O(N)
+    // SC: O(N)
+    // but we could optimize this solution because the problem dont need us to provide the whole inorder stakc, it just want to know if this is a BST. So we could keep the topvalue of inStack for check in stead of the whole inStack.
+    private boolean verifyPreorderByOneStack(int[] preorder){
+        // special case
+        if(preorder == null || preorder.length == 0 || preorder.length == 1){
+            return true;
+        }
+
+        // we use a value to help us keep the top value of inStack, because the inStack is empty at start so we make this value be the min
+        int topInInStack = Integer.MIN_VALUE;
+
+        Deque<Integer> preStack = new ArrayDeque<>();
+
+        for(int i = 0; i < preorder.length; i++){
+            int curElem = preorder[i];
+
+            // if curElem is smaller than top in inStack, then inorder is violated.
+            if(curElem < topInInStack){
+                return false;
+            }
+
+            while(!preStack.isEmpty() && curElem > preStack.peek()){
+                int preTop = preStack.pop();
+
+                // the same, if preTop is smaller than top in inStack, then inorder is violated
+                if(preTop < topInInStack){
+                    return false;
+                }
+
+                // valid, kee the new top in inStack
+                topInInStack = preTop;
+            }
+
+            // after process all smaller nodes, push curElem to preTop
+            preStack.push(curElem);
+        }
+
+        return true;
+    }
+
+    // TC: O(N)
+    // SC: O(N)
+    // 利用preorder和inorder的关系：对于preorder，我们可以通过利用两个stack可以将其转换成inorder traversal。
+    // 那么如果我们把preorder array转成inorder array，对于BST来说，inorder必须是ascengding的顺序，这样inorder是BST，推出preorder是BST
+    private boolean verifyPreorderByTwoStack(int[] preorder){
+        // special case
+        if(preorder == null || preorder.length == 0 || preorder.length == 1){
+            return true;
+        }
+
+        // we use two stacks to help us, preStack to store all left node, meaning the preStack is ascending from top to bottom. if curElem is bigger than preStack.top, then we could not push this curElem in because this should be somenode's right node, so we pop all inorder(ascending) nodes smaller than curElem to inStack, then our inStack is descending from top to bottom.
+        // then we push curElem to preStack
+        // at last, we push all nodes in preStack(must be ascending) to inStack
+        // then we check inStack is inorder or not to verify BST
+        Deque<Integer> preStack = new ArrayDeque<>();
+        Deque<Integer> inStack = new ArrayDeque<>();
+
+        for(int i = 0; i < preorder.length; i++){
+            int curElem = preorder[i];
+
+            // push all nodes smaller than curElem to inStack
+            while(!preStack.isEmpty() && curElem > preStack.peek()){
+                int preTop = preStack.pop();
+
+                // put to inStack
+                inStack.push(preTop);
+            }
+
+            // after process all its left nodes, curElem push to preOrder
+            preStack.push(curElem);
+        }
+
+        // !!! push everything left in preStack to inStack
+        while(!preStack.isEmpty()){
+            inStack.push(preStack.pop());
+        }
+
+        // check if inStack is valid
+        int upperNum = Integer.MAX_VALUE;
+        while(!inStack.isEmpty()){
+            if(upperNum < inStack.peek()){
+                return false;
+            }
+
+            upperNum = inStack.pop();
+        }
+
+        return true;
     }
 
     // TC: O(N^2) worst, O(N*log(N)) if the BST is balanced
