b45ea444b8e26709b824226bcfa454d28e106570
Kenshin <hi_appler@126.com>
2019-04-12 13:40:46 -0700

    340. Longest Substring with At Most K Distinct Characters

---
+++ src/main/java/com/myleetcode/sliding_window/longest_substring_with_at_most_k_distinct_characters/LongestSubstringWithAtMostKDistinctCharacters.java
@@ -0,0 +1,49 @@
+package com.myleetcode.sliding_window.longest_substring_with_at_most_k_distinct_characters;
+
+public class LongestSubstringWithAtMostKDistinctCharacters {
+    /**
+     * 340. Longest Substring with At Most K Distinct Characters
+     * Hard
+     *
+     * 492
+     *
+     * 16
+     *
+     * Favorite
+     *
+     * Share
+     * Given a string, find the length of the longest substring T that contains at most k distinct characters.
+     *
+     * Example 1:
+     *
+     * Input: s = "eceba", k = 2
+     * Output: 3
+     * Explanation: T is "ece" which its length is 3.
+     * Example 2:
+     *
+     * Input: s = "aa", k = 1
+     * Output: 2
+     * Explanation: T is "aa" which its length is 2.
+     */
+
+    /**
+     * Hash Table
+     * Two Pointers
+     * String
+     * Sliding Window
+     */
+
+    /**
+     * Facebook
+     * |
+     * 11
+     *
+     * Google
+     * |
+     * 8
+     *
+     * Amazon
+     * |
+     * 6
+     */
+}
---
+++ src/main/java/com/myleetcode/sliding_window/longest_substring_with_at_most_k_distinct_characters/Solution.java
@@ -0,0 +1,62 @@
+package com.myleetcode.sliding_window.longest_substring_with_at_most_k_distinct_characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        return lengthOfLongestSubstringKDistinctBySlidingWindow(s, k);
+    }
+
+    // intuition: this is a more generalized situation derived from problem: 159. Longest Substring with At Most Two Distinct Characters
+    // but the thoughts behind is same
+    private int lengthOfLongestSubstringKDistinctBySlidingWindow(String s, int k){
+        // special case
+        if(s == null || k <= 0){
+            return 0;
+        }
+        if(s.length() <= k){
+            return s.length();
+        }
+
+        // map to count distinct char num
+        Map<Character, Integer> charNumMap = new HashMap<>();
+
+        char[] charArray = s.toCharArray();
+
+        // window left
+        int leftP = 0;
+
+        int maxLen = Integer.MIN_VALUE;
+
+        for(int i = 0; i < charArray.length; i++){
+            // current char add to map
+            charNumMap.put(charArray[i], charNumMap.getOrDefault(charArray[i], 0) + 1);
+
+            // if window is valid, update maxLen
+            if(charNumMap.size() <= k){
+                maxLen = Math.max(maxLen, i - leftP + 1); // here use index to caculate length is safe and fast
+            }else{
+                // shrink window
+                while(leftP < i){
+                    // get char at leftP
+                    char charLeftP = charArray[leftP];
+                    // reduce its number in map
+                    charNumMap.put(charLeftP, charNumMap.get(charLeftP) - 1);
+                    // move forward leftP
+                    leftP++;
+
+                    // check if window valid
+                    if(charNumMap.get(charLeftP) == 0){
+                        charNumMap.remove(charLeftP);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return maxLen;
+
+    }
+
+}
