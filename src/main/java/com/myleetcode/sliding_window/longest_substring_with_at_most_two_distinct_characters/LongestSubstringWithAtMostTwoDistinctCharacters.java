b4cf419b9563007e11549fd172e7057b56e04a16
Kenshin <hi_appler@126.com>
2019-04-12 13:38:05 -0700

    159. Longest Substring with At Most Two Distinct Characters

---
+++ src/main/java/com/myleetcode/sliding_window/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithAtMostTwoDistinctCharacters.java
@@ -0,0 +1,45 @@
+package com.myleetcode.sliding_window.longest_substring_with_at_most_two_distinct_characters;
+
+public class LongestSubstringWithAtMostTwoDistinctCharacters {
+    /**
+     * 159. Longest Substring with At Most Two Distinct Characters
+     * Hard
+     *
+     * 520
+     *
+     * 9
+     *
+     * Favorite
+     *
+     * Share
+     * Given a string s , find the length of the longest substring t  that contains at most 2 distinct characters.
+     *
+     * Example 1:
+     *
+     * Input: "eceba"
+     * Output: 3
+     * Explanation: t is "ece" which its length is 3.
+     * Example 2:
+     *
+     * Input: "ccaabbb"
+     * Output: 5
+     * Explanation: t is "aabbb" which its length is 5.
+     */
+
+    /**
+     * Hash Table
+     * Two Pointers
+     * String
+     * Sliding Window
+     */
+
+    /**
+     * Google
+     * |
+     * 19
+     *
+     * Amazon
+     * |
+     * 6
+     */
+}
---
+++ src/main/java/com/myleetcode/sliding_window/longest_substring_with_at_most_two_distinct_characters/Solution.java
@@ -0,0 +1,124 @@
+package com.myleetcode.sliding_window.longest_substring_with_at_most_two_distinct_characters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        // return lengthOfLongestSubstringTwoDistinctBySlidingWindow(s);
+        return lengthOfLongestSubstringTwoDistinctBySlidingWindowII(s);
+    }
+
+    // optimize, template it
+    // actually there's a solution with int[] array instead of map to count the char, that will be much faster and spend less space, but I dont like it. there will be many logics to caculates num of chars and hard to remeber.
+    private int lengthOfLongestSubstringTwoDistinctBySlidingWindowII(String s){
+        if(s == null){
+            return 0;
+        }
+        if(s.length() <= 2){
+            return s.length();
+        }
+
+        // map to count
+        Map<Character, Integer> charNumMap = new HashMap<>();
+
+        char[] charArray = s.toCharArray();
+
+        // two pointers to form window
+        int leftP = 0;
+        // int rightP = 0;
+
+        // result
+        int maxLen = Integer.MIN_VALUE;
+
+        // outter loop, spread window and when necessary we shrink window
+        for(int i = 0; i < charArray.length; i++){ //i is rightP
+            // spread window
+            charNumMap.put(charArray[i], charNumMap.getOrDefault(charArray[i], 0) +1);
+
+            // update maxLen if have less than or equal to 2 keys
+            if(charNumMap.keySet().size() <= 2){
+                maxLen = Math.max(maxLen, i - leftP + 1);
+            }
+
+            // if has more than two keys in map, shrink window to make it's valid
+            if(charNumMap.keySet().size() > 2){
+                // loop to shrink window
+                while(leftP < i){
+                    // get current leftP char
+                    char charLeftP = charArray[leftP];
+                    // move forward
+                    leftP++;
+
+                    // reduce num and if one distinct char key has 0 num, remove it and we are good then
+                    charNumMap.put(charLeftP, charNumMap.get(charLeftP) - 1);
+                    if(charNumMap.get(charLeftP) == 0){
+                        charNumMap.remove(charLeftP);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return maxLen;
+
+    }
+
+    // TC: O(N)
+    // SC: O(N)
+    // intuition: sliding window. use leftP and rightP to build a window, use a map to store what chars we find now, normally leftP points to a char A and rightP points to another different char B.
+    // rightP move to end of S, if it encounter a new char which is different to current two, ie not contained in current map, we put it into map and shrink window, move leftP to the first B char, this we could do by leftP + map.get(A) this is the value's usage in map. then we start spread window again.
+    private int lengthOfLongestSubstringTwoDistinctBySlidingWindow(String s){
+        if(s == null){
+            return 0;
+        }
+        if(s.length() <= 2){
+            return s.length();
+        }
+
+        Map<Character, Integer> charNumMap = new HashMap<>();
+        char[] charArray = s.toCharArray();
+        int leftP = 0;
+        // int rightP = 0;
+        int maxLen = Integer.MIN_VALUE;
+        for(int i = 0; i < charArray.length; i++){ //i is rightP
+            // if we have less than or euqal to 2 keys in map, we need to spread window
+            if(charNumMap.keySet().size() <= 2){
+                charNumMap.put(charArray[i], charNumMap.getOrDefault(charArray[i], 0) +1);
+
+                // update maxLen if have 2 keys
+                if(charNumMap.keySet().size() <= 2){
+                    // this costs too much, but dont get good idea
+                    // int count = 0;
+                    // for(char key: charNumMap.keySet()){
+                    //     count += charNumMap.get(key);
+                    // }
+                    // actually we could use the i - leftP + 1
+
+                    maxLen = Math.max(maxLen, i - leftP + 1);
+                }
+            }
+
+            // if has more than two keys in map, shrink window
+            if(charNumMap.keySet().size() > 2){
+                // otherwise we shrink window
+                while(leftP < i){
+                    // get current leftP char
+                    char charLeftP = charArray[leftP];
+                    // move
+                    leftP++;
+
+                    // reduce num
+                    charNumMap.put(charLeftP, charNumMap.get(charLeftP) - 1);
+                    if(charNumMap.get(charLeftP) == 0){
+                        charNumMap.remove(charLeftP);
+                        break;
+                    }
+                }
+            }
+        }
+
+        return maxLen;
+
+    }
+}
