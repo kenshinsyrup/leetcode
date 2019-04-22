6393803d5d94055bd353a285d951f295a7135cbb
Kenshin <hi_appler@126.com>
2019-04-22 10:50:11 -0700

    673. Number of Longest Increasing Subsequence

---
+++ src/main/java/com/myleetcode/dynamic_program/number_of_longest_increasing_subsequence/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,41 @@
+package com.myleetcode.dynamic_program.number_of_longest_increasing_subsequence;
+
+public class NumberOfLongestIncreasingSubsequence {
+    /**
+     * 673. Number of Longest Increasing Subsequence
+     * Medium
+     *
+     * 753
+     *
+     * 53
+     *
+     * Favorite
+     *
+     * Share
+     * Given an unsorted array of integers, find the number of longest increasing subsequence.
+     *
+     * Example 1:
+     * Input: [1,3,5,4,7]
+     * Output: 2
+     * Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
+     * Example 2:
+     * Input: [2,2,2,2,2]
+     * Output: 5
+     * Explanation: The length of longest continuous increasing subsequence is 1, and there are 5 subsequences' length is 1, so output 5.
+     * Note: Length of the given array will be not exceed 2000 and the answer is guaranteed to be fit in 32-bit signed int.
+     */
+
+    /**
+     * Dynamic Programming
+     */
+
+    /**
+     * Facebook
+     * |
+     * 3
+     *
+     * Amazon
+     * |
+     * 2
+     */
+}
