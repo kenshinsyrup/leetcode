fe2af63c00c27e7a1a6489d1d2b41f338b83433d
Kenshin <hi_appler@126.com>
2019-04-30 16:56:20 -0700

    182. Duplicate Emails

---
+++ src/main/java/com/myleetcode/database/duplicate_emails/DuplicateEmails.java
@@ -0,0 +1,34 @@
+package com.myleetcode.database.duplicate_emails;
+
+public class DuplicateEmails {
+    /**
+     * 182. Duplicate Emails
+     * Easy
+     *
+     * 266
+     *
+     * 15
+     *
+     * Favorite
+     *
+     * Share
+     * SQL Schema
+     * Write a SQL query to find all duplicate emails in a table named Person.
+     *
+     * +----+---------+
+     * | Id | Email   |
+     * +----+---------+
+     * | 1  | a@b.com |
+     * | 2  | c@d.com |
+     * | 3  | a@b.com |
+     * +----+---------+
+     * For example, your query should return the following for the above table:
+     *
+     * +---------+
+     * | Email   |
+     * +---------+
+     * | a@b.com |
+     * +---------+
+     * Note: All emails are in lowercase.
+     */
+}
