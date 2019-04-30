2446d3d1f91aae004157ab52e0ce7b270cbe11b9
Kenshin <hi_appler@126.com>
2019-04-30 16:14:56 -0700

    176. Second Highest Salary

---
+++ src/main/java/com/myleetcode/database/second_highest_salary/SecondHighestSalary.java
@@ -0,0 +1,59 @@
+package com.myleetcode.database.second_highest_salary;
+
+public class SecondHighestSalary {
+    /**
+     * 176. Second Highest Salary
+     * Easy
+     *
+     * 473
+     *
+     * 238
+     *
+     * Favorite
+     *
+     * Share
+     * SQL Schema
+     * Write a SQL query to get the second highest salary from the Employee table.
+     *
+     * +----+--------+
+     * | Id | Salary |
+     * +----+--------+
+     * | 1  | 100    |
+     * | 2  | 200    |
+     * | 3  | 300    |
+     * +----+--------+
+     * For example, given the above Employee table, the query should return 200 as the second highest salary. If there is no second highest salary, then the query should return null.
+     *
+     * +---------------------+
+     * | SecondHighestSalary |
+     * +---------------------+
+     * | 200                 |
+     * +---------------------+
+     */
+
+    /**
+     * Amazon
+     * |
+     * 10
+     *
+     * Google
+     * |
+     * 4
+     *
+     * Adobe
+     * |
+     * 4
+     *
+     * Apple
+     * |
+     * 3
+     *
+     * Facebook
+     * |
+     * 2
+     *
+     * Microsoft
+     * |
+     * 2
+     */
+}
