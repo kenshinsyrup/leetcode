2153f112b9ff05281ddc82ac00e789bc9bc8e32e
Kenshin <hi_appler@126.com>
2019-04-30 16:49:34 -0700

    181. Employees Earning More Than Their Managers

---
+++ src/main/java/com/myleetcode/database/employees_earning_more_than_their_managers/EmployeesEarningMoreThanTheirManagers.java
@@ -0,0 +1,48 @@
+package com.myleetcode.database.employees_earning_more_than_their_managers;
+
+public class EmployeesEarningMoreThanTheirManagers {
+    /**
+     * 181. Employees Earning More Than Their Managers
+     * Easy
+     *
+     * 330
+     *
+     * 35
+     *
+     * Favorite
+     *
+     * Share
+     * SQL Schema
+     * The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+     *
+     * +----+-------+--------+-----------+
+     * | Id | Name  | Salary | ManagerId |
+     * +----+-------+--------+-----------+
+     * | 1  | Joe   | 70000  | 3         |
+     * | 2  | Henry | 80000  | 4         |
+     * | 3  | Sam   | 60000  | NULL      |
+     * | 4  | Max   | 90000  | NULL      |
+     * +----+-------+--------+-----------+
+     * Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+     *
+     * +----------+
+     * | Employee |
+     * +----------+
+     * | Joe      |
+     * +----------+
+     */
+
+    /**
+     * Yahoo
+     * |
+     * 2
+     *
+     * Amazon
+     * |
+     * 2
+     *
+     * Facebook
+     * |
+     * 2
+     */
+}
