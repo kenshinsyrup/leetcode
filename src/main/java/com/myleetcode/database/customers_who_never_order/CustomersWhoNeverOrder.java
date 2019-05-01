c8aa56e009a40d7174b5caacf13bddb8469b200e
Kenshin <hi_appler@126.com>
2019-04-30 17:02:36 -0700

    183. Customers Who Never Order

---
+++ src/main/java/com/myleetcode/database/customers_who_never_order/CustomersWhoNeverOrder.java
@@ -0,0 +1,51 @@
+package com.myleetcode.database.customers_who_never_order;
+
+public class CustomersWhoNeverOrder {
+    /**
+     * 183. Customers Who Never Order
+     * Easy
+     *
+     * 217
+     *
+     * 25
+     *
+     * Favorite
+     *
+     * Share
+     * SQL Schema
+     * Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.
+     *
+     * Table: Customers.
+     *
+     * +----+-------+
+     * | Id | Name  |
+     * +----+-------+
+     * | 1  | Joe   |
+     * | 2  | Henry |
+     * | 3  | Sam   |
+     * | 4  | Max   |
+     * +----+-------+
+     * Table: Orders.
+     *
+     * +----+------------+
+     * | Id | CustomerId |
+     * +----+------------+
+     * | 1  | 3          |
+     * | 2  | 1          |
+     * +----+------------+
+     * Using the above tables as example, return the following:
+     *
+     * +-----------+
+     * | Customers |
+     * +-----------+
+     * | Henry     |
+     * | Max       |
+     * +-----------+
+     */
+
+    /**
+     * Amazon
+     * |
+     * 2
+     */
+}
