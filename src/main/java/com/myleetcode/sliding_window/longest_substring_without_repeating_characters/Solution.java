288f1611bfc12eb6930051df249325b4b1cef8e3
Kenshin <hi_appler@126.com>
2019-05-06 15:16:00 -0700

    optimize: 3. Longest Substring Without Repeating Characters

--- src/main/java/com/myleetcode/sliding_window/longest_substring_without_repeating_characters/Solution.java
+++ src/main/java/com/myleetcode/sliding_window/longest_substring_without_repeating_characters/Solution.java
@@ -7,12 +7,56 @@ import java.util.Set;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        return lenghtOfLongestSubstringBySlidingWindow(s);
+        // return lenghtOfLongestSubstringBySlidingWindow(s);
+        return lenghtOfLongestSubstringBySlidingWindowII(s);
+    }
+
+    // 3, 159, 340, 992
+    // https://leetcode.com/problems/subarrays-with-k-different-integers/discuss/235002/One-code-template-to-solve-all-of-these-problems!
+    // use the at most K distincts template
+    private int lenghtOfLongestSubstringBySlidingWindowII(String str){
+        if(str == null || str.length() == 0){
+            return 0;
+        }
+
+        int maxLen = 0;
+
+        Map<Character, Integer> charNumMap = new HashMap<>();
+
+        int len = str.length();
+        int left = 0;
+        int distinct = 0;
+        for(int i = 0; i < len; i++){
+            // counting the # of char
+            charNumMap.put(str.charAt(i), charNumMap.getOrDefault(str.charAt(i), 0) + 1);
+            // meet first time
+            if(charNumMap.get(str.charAt(i)) == 1){
+                distinct++;
+            }
+
+            // shrink window, here the condition needs a little thoughts: when we need shrink window, means there are repeated elem in the window[left:i], so must have this: distinct < i-left+1. (actually distinct only have two conditions, < i-left+1 or ==i-left+1, it's impossible >)
+            while(left <= i && distinct < i - left + 1){
+                charNumMap.put(str.charAt(left), charNumMap.get(str.charAt(left)) - 1);
+
+                // if a distinct char is removed
+                if(charNumMap.get(str.charAt(left)) == 0){
+                    distinct--;
+                }
+
+                left++;
+            }
+
+            // get the window length, check if need update maxLen
+            maxLen = Math.max(maxLen, i - left + 1);
+        }
+
+        return maxLen;
+
     }
 
 
     // TC: O(N)
-    // SC: O(min(M, N)), where M is the longest length of Set, actually the Set's length is at most 26
+    // SC: O(min(M, N)), whrer M is the longest length of Set, actually the Set's length is at most 26
     // intuition: Sliding Window
     private int lenghtOfLongestSubstringBySlidingWindow(String str){
         if(str == null || str.length() == 0){
@@ -28,7 +72,7 @@ class Solution {
         int right = 0;
         while(left <= right && right < len){
             char ch = str.charAt(right);
-            // valid, expand
+            // valid, spand
             if(!charSet.contains(ch)){
                 // !!! must first caculate the length of current string, then make right++(of course, otherwise your lenght is not correct)
                 maxLen = Math.max(maxLen, right - left + 1);
