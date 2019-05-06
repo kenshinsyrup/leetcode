33d11e859200d5ca78182bc8069a5c74ac9ff2cd
Kenshin <hi_appler@126.com>
2019-05-06 12:37:28 -0700

    212. Word Search II

---
+++ src/main/java/com/myleetcode/trie/word_search_ii/WordSearchII.java
@@ -0,0 +1,75 @@
+package com.myleetcode.trie.word_search_ii;
+
+public class WordSearchII {
+    /**
+     * 212. Word Search II
+     * Hard
+     *
+     * 1051
+     *
+     * 68
+     *
+     * Favorite
+     *
+     * Share
+     * Given a 2D board and a list of words from the dictionary, find all words in the board.
+     *
+     * Each word must be constructed from letters of sequentially adjacent cell, where "adjacent" cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+     *
+     *
+     *
+     * Example:
+     *
+     * Input:
+     * board = [
+     *   ['o','a','a','n'],
+     *   ['e','t','a','e'],
+     *   ['i','h','k','r'],
+     *   ['i','f','l','v']
+     * ]
+     * words = ["oath","pea","eat","rain"]
+     *
+     * Output: ["eat","oath"]
+     *
+     *
+     * Note:
+     *
+     * All inputs are consist of lowercase letters a-z.
+     * The values of words are distinct.
+     */
+
+    /**
+     * Trie
+     * Backtracking
+     */
+
+    /**
+     * Amazon
+     * |
+     * 17
+     *
+     * Uber
+     * |
+     * 5
+     *
+     * Microsoft
+     * |
+     * 4
+     *
+     * Google
+     * |
+     * 4
+     *
+     * Facebook
+     * |
+     * 2
+     *
+     * Airbnb
+     * |
+     * 2
+     *
+     * Apple
+     * |
+     * 2
+     */
+}
