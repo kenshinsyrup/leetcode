262c7a49ff7e1ab99c74e1740c773bf45b27a736
Kenshin <hi_appler@126.com>
2019-05-09 16:29:21 -0700

    42. Trapping Rain Water

---
+++ src/main/java/com/myleetcode/monotonic_stack/trapping_rain_water/TrappingRainWater.java
@@ -0,0 +1,104 @@
+package com.myleetcode.monotonic_stack.trapping_rain_water;
+
+public class TrappingRainWater{
+    /**
+     * 42. Trapping Rain Water
+     Hard
+
+     2819
+
+     49
+
+     Favorite
+
+     Share
+     Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+
+     The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
+
+     Example:
+
+     Input: [0,1,0,2,1,0,1,3,2,1,2,1]
+     Output: 6
+
+     */
+
+    /**
+     * Array
+     * Two Pointers
+     * Stack
+     */
+
+    /** companies
+     * 0 ~ 6 months6 months ~ 1 year1 year ~ 2 years
+
+     Goldman Sachs
+     |
+     23
+
+     Amazon
+     |
+     20
+
+     Facebook
+     |
+     17
+
+     Lyft
+     |
+     16
+
+     Bloomberg
+     |
+     14
+
+     Google
+     |
+     13
+
+     Microsoft
+     |
+     7
+
+     Uber
+     |
+     5
+
+     Apple
+     |
+     5
+
+     Airbnb
+     |
+     4
+
+     Adobe
+     |
+     3
+
+     Dataminr
+     |
+     2
+
+     Grab
+     |
+     2
+
+     Qualtrics
+     |
+     2
+
+     Nvidia
+     |
+     2
+
+     Salesforce
+     |
+     2
+
+     Oracle
+     |
+     2
+     */
+}
