4f9f3ec6af166dfd8156cbc487d7688fb1948726
Kenshin <hi_appler@126.com>
2019-06-13 17:22:59 -0700

    PQ: 295. Find Median from Data Stream

---
+++ src/main/java/com/myleetcode/priority_queue/find_median_from_data_stream/FindMedianFromDataStream.java
@@ -0,0 +1,97 @@
+package com.myleetcode.priority_queue.find_median_from_data_stream;
+
+public class FindMedianFromDataStream {
+    /**
+     * 295. Find Median from Data Stream
+     * Hard
+     *
+     * 1153
+     *
+     * 23
+     *
+     * Favorite
+     *
+     * Share
+     * Median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value. So the median is the mean of the two middle value.
+     *
+     * For example,
+     * [2,3,4], the median is 3
+     *
+     * [2,3], the median is (2 + 3) / 2 = 2.5
+     *
+     * Design a data structure that supports the following two operations:
+     *
+     * void addNum(int num) - Add a integer number from the data stream to the data structure.
+     * double findMedian() - Return the median of all elements so far.
+     *
+     *
+     * Example:
+     *
+     * addNum(1)
+     * addNum(2)
+     * findMedian() -> 1.5
+     * addNum(3)
+     * findMedian() -> 2
+     *
+     *
+     * Follow up:
+     *
+     * If all integer numbers from the stream are between 0 and 100, how would you optimize it?
+     * If 99% of all integer numbers from the stream are between 0 and 100, how would you optimize it?
+     */
+
+    /**
+     * Heap
+     * Design
+     */
+
+    /**
+     * Amazon
+     * |
+     * 25
+     *
+     * Microsoft
+     * |
+     * 6
+     *
+     * Google
+     * |
+     * 6
+     *
+     * Twitter
+     * |
+     * 4
+     *
+     * Facebook
+     * |
+     * 3
+     *
+     * Expedia
+     * |
+     * 3
+     *
+     * Uber
+     * |
+     * 3
+     *
+     * Apple
+     * |
+     * 2
+     *
+     * Snapchat
+     * |
+     * 2
+     *
+     * Goldman Sachs
+     * |
+     * 2
+     *
+     * Pony.ai
+     * |
+     * 2
+     *
+     * Netflix
+     * |
+     * 2
+     */
+}
---
+++ src/main/java/com/myleetcode/priority_queue/find_median_from_data_stream/Solution.java
@@ -0,0 +1,111 @@
+package com.myleetcode.priority_queue.find_median_from_data_stream;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+class MedianFinder {
+
+    // solution 2: Two Heaps
+    // TC: O(logN)
+    // SC: O(N)
+    // the thought is: assume we have a Sorted List, then, at the median pos, we could split the List to two parts, less or equals to median part, larger than median part
+    // since we want to caculate the new median after add a new num, so we only need to access at most two num from those two parts(one in one part), the largest one in the left part and the smallest one in the right part(attention, we say, at most)
+    // then, we could use a MaxHeap for the left part, and MinHeap for the right part
+    // add: offer num to Left MaxHeap, then we get a new left part; now left is not valid(because at first our left and right are equal or left has one more elem than right, both situations are valid), so we need poll the Max from the LeftMaxHeap and offer it to the Righit MinHeap. Now, we have two parts sorted and we could access the largest elem in left part and smallest elem in right part, so we could get the new median
+    // mdeian: according to the total # in left and right part. if odd, return the top of Left MaxHeap; if even, return the average of the top of Left MaxHeap and the top of Right MinHeap
+
+    PriorityQueue<Integer> leftPQ;
+    PriorityQueue<Integer> rightPQ;
+
+    /** initialize your data structure here. */
+    public MedianFinder() {
+        // MaxHeap
+        leftPQ = new PriorityQueue<>(new Comparator<Integer>(){
+            public int compare(Integer n1, Integer n2){
+                return n2 - n1;
+            }
+        });
+
+
+        // MinHeap is default
+        rightPQ = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+        // offer the num to Left MaxHeap
+        leftPQ.offer(num);
+
+        // offer the top of Left MaxHeap to Right MinHeap
+        rightPQ.offer(leftPQ.poll());
+
+        // !!! must keep the left part is more than right part, this makes when total # is odd, the median is in the top of left
+        if(leftPQ.size() < rightPQ.size()){
+            leftPQ.offer(rightPQ.poll());
+        }
+    }
+
+    // TC: O(1)
+    // SC: O(1)
+    public double findMedian() {
+        int len = leftPQ.size() + rightPQ.size();
+
+        if(len % 2 == 0){
+            return (leftPQ.peek() + rightPQ.peek()) / 2.0;
+        }
+        return leftPQ.peek() * 1.0;
+    }
+
+
+//     // solution 1: Insertion Sort
+//     // TC: O(N)
+//     // SC: O(N)
+//     // add: use BS to find the pos to put the new num, then shift all num back by 1 in original list, then put the new num in
+//     // median: odd is the pos at len/2, even is the average of len/2 and len/2-1
+
+//     List<Integer> numList;
+
+//     /** initialize your data structure here. */
+//     public MedianFinder() {
+//         this.numList = new ArrayList<>();
+//     }
+
+//     // TC: O(N)
+//     // SC: O(N)
+//     public void addNum(int num) {
+//         int len = this.numList.size();
+
+//         int start = 0;
+//         int end = len - 1;
+//         while(start <= end){
+//             int mid = start + ((end - start) >> 1);
+
+//             // we need the first No Less Than num pos
+//             if(this.numList.get(mid) < num){
+//                 start = mid + 1;
+//             }else{
+//                 end = mid - 1;
+//             }
+//         }
+
+//         // add the num to the First No Less Than num Pos, the nums in original List will be shifted back by 1, cost O(N)
+//         this.numList.add(start, num);
+//     }
+
+//     // TC: O(1)
+//     // SC: O(1)
+//     public double findMedian() {
+//         int len = this.numList.size();
+
+//         if(len % 2 == 0){
+//             return (this.numList.get(len / 2) + this.numList.get(len / 2 - 1)) / 2.0;
+//         }
+//         return this.numList.get(len / 2) * 1.0;
+//     }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
