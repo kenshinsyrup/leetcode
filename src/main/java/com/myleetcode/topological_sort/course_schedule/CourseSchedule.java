c6a8e3f3b74857953d0f4695fedd382648c6e84c
Kenshin <hi_appler@126.com>
2019-06-17 15:37:46 -0700

    DAG, Topological Sort, Optimize: 207. Course Schedule

---
+++ src/main/java/com/myleetcode/topological_sort/course_schedule/CourseSchedule.java
@@ -0,0 +1,80 @@
+package com.myleetcode.topological_sort.course_schedule;
+
+public class CourseSchedule {
+    /**
+     * 207. Course Schedule
+     * Medium
+     *
+     * 1809
+     *
+     * 88
+     *
+     * Favorite
+     *
+     * Share
+     * There are a total of n courses you have to take, labeled from 0 to n-1.
+     *
+     * Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+     *
+     * Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+     *
+     * Example 1:
+     *
+     * Input: 2, [[1,0]]
+     * Output: true
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0. So it is possible.
+     * Example 2:
+     *
+     * Input: 2, [[1,0],[0,1]]
+     * Output: false
+     * Explanation: There are a total of 2 courses to take.
+     *              To take course 1 you should have finished course 0, and to take course 0 you should
+     *              also have finished course 1. So it is impossible.
+     * Note:
+     *
+     * The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+     * You may assume that there are no duplicate edges in the input prerequisites.
+     */
+
+    /**
+     * DFS
+     * BFS
+     * Topological Sort
+     * Graph
+     */
+
+    /**
+     * Amazon
+     * |
+     * 23
+     *
+     * Uber
+     * |
+     * 6
+     *
+     * Google
+     * |
+     * 5
+     *
+     * Intuit
+     * |
+     * 3
+     *
+     * Salesforce
+     * |
+     * 3
+     *
+     * Facebook
+     * |
+     * 2
+     *
+     * Nutanix
+     * |
+     * 2
+     *
+     * eBay
+     * |
+     * 2
+     */
+}
---
+++ src/main/java/com/myleetcode/topological_sort/course_schedule/Solution.java
@@ -0,0 +1,81 @@
+package com.myleetcode.topological_sort.course_schedule;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        // special case
+        if(numCourses < 0){
+            return false;
+        }
+        // LeetCode: 根据题目，prerequisites只是may，所以如果不存在的话，我们应该更无所顾虑的完成了所有的课毕竟没有依赖了，所以这里的case一定是true
+        if(prerequisites == null || prerequisites.length == 0){
+            return true;
+        }
+
+        return canFinisiByTopologyAndDFS(numCourses, prerequisites);
+    }
+
+    // TC: O(M + N), M is |E|, N is |V|
+    // SC: O(N)
+    // intuition: this problem dont need us to give the Topological Sort result, it only asks if there's a Topological Sort, so, this is actually a Verify DAG problem.
+    // we only need to check if the G is a DAG, if it is, then it has Topological Sort, means we could finish all courses.
+    // to Verify DAG, use the Three Color algo
+
+    // Topology
+    // https://leetcode.com/problems/course-schedule/discuss/58532/5ms-DFS-beat-98-and-9ms-BFS-in-java
+    // https://efficientcodeblog.wordpress.com/2017/11/28/topological-sort-dfs-bfs-and-dag/
+    //https://www.khanacademy.org/computing/computer-science/algorithms/graph-representation/a/representing-graphs
+    private boolean canFinisiByTopologyAndDFS(int numCourses, int[][] prerequisites){
+        // init graph, normalize the int[][] prerequisites to graph represented by Adjacent List
+        Map<Integer, List<Integer>> graph = new HashMap<>(); // this problem we could use List<List<Integer>> graph because the courses are integer
+        for(int[] pre: prerequisites){
+            graph.putIfAbsent(pre[1], new ArrayList<>());
+            graph.get(pre[1]).add(pre[0]);
+        }
+
+        // three colors for three status: 0 is new node, 1 is visiting node, -1 is visited node
+        int[] colors = new int[numCourses];
+
+        // DFSAll, if has cycle, then not DAG, return false
+        for(int i = 0; i < numCourses; i++){
+            if(hasCycle(graph, i, colors)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean hasCycle(Map<Integer, List<Integer>> graph, int courseIdx, int[] colors){
+        // set status to visiting for current course
+        colors[courseIdx] = 1;
+
+        // process children nodes, here should be careful that graph is the prerequisite graph, so maybe there's no children course list of current courseIdx, so we use getOrDefault to avoid the null pointer error in the for loop
+        List<Integer> nextCourseList = graph.getOrDefault(courseIdx, new ArrayList<>());
+        for(int i = 0; i < nextCourseList.size(); i++){
+            // children courses
+            int nextCourseIdx = nextCourseList.get(i);
+
+            // check color:
+            // if visiting, means has cycle, true
+            // if new node and has cycle, true
+            if(colors[nextCourseIdx] == 1){
+                return true;
+            }else if(colors[nextCourseIdx] == 0){
+                if(hasCycle(graph, nextCourseIdx, colors)){
+                    return true ;
+                }
+            }
+        }
+
+        // set status to visited for current course
+        colors[courseIdx] = -1;
+
+        return false;
+    }
+
+}
