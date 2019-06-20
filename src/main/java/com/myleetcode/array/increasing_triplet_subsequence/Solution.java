8ee6c210a6bd3cba60f016fd3ba5789fec532268
Kenshin <hi_appler@126.com>
2019-06-20 13:22:36 -0700

    add some comments about Patience Sort: 334. Increasing Triplet Subsequence

--- src/main/java/com/myleetcode/array/increasing_triplet_subsequence/Solution.java
+++ src/main/java/com/myleetcode/array/increasing_triplet_subsequence/Solution.java
@@ -1,8 +1,14 @@
 package com.myleetcode.array.increasing_triplet_subsequence;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 class Solution {
     public boolean increasingTriplet(int[] nums) {
         return increasingTripletByPatienceSorting(nums);
+        // return increasingTripletByClassicPatienceSorting(nums);
     }
 
     // actually this is a special case of problem 300. Longest Increasing Subsequence. so definitly we could use DP to get the LIS and check if the length larger than 3 or not. BUT, it will be O(N^2) TC which is even slower than Sort and Scan solution.
@@ -10,8 +16,7 @@ class Solution {
     // solution: with Patience Sorting
     // TC: O(N)
     // SC: O(1)
-    // https://leetcode.com/problems/increasing-triplet-subsequence/discuss/79023/Just-a-simplified-version-of-patient-sort.
-    // http://wordaligned.org/articles/patience-sort
+    // https://leetcode.com/problems/increasing-triplet-subsequence/discuss/79000/My-accepted-JAVA-solution-for-this-question-only-7-lines-clear-and-concise.
     private boolean increasingTripletByPatienceSorting(int[] nums){
         if(nums == null || nums.length <= 2){
             return false;
@@ -26,6 +31,39 @@ class Solution {
             }else if(num <= secondMin){
                 secondMin = num;
             }else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // this is the classical form to write the Patience Sort
+    // TC: O(N)
+    // SC: O(N), worst case, all nums are desc order so they are in the first pile
+    private boolean increasingTripletByClassicPatienceSorting(int[] nums){
+        List<Deque<Integer>> pileList = new ArrayList<>();
+
+        for(int num: nums){
+            // temp pile
+            Deque<Integer> tempPile = new ArrayDeque<>();
+            tempPile.push(num);
+
+            // 1 put the left most pile that's top num is smaller num than current num
+            for(Deque<Integer> pile: pileList){
+                if(tempPile.peek() <= pile.peek()){
+                    pile.push(tempPile.pop());
+                    break;
+                }
+            }
+
+            // 2 if not find such pile, then this is a new pile, add to list
+            if(!tempPile.isEmpty()){
+                pileList.add(tempPile);
+            }
+
+            // check length of list
+            if(pileList.size() >= 3){
                 return true;
             }
         }
