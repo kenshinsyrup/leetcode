91398568d547d4cc1d7bafde224093bbd8faaadf
Kenshin <hi_appler@126.com>
2019-06-28 11:14:31 -0700

    Binary Indexed Tree: 308. Range Sum Query 2D - Mutable

--- src/main/java/com/myleetcode/design/range_sum_query_2d_mutable/Solution.java
+++ src/main/java/com/myleetcode/design/range_sum_query_2d_mutable/Solution.java
@@ -1,79 +1,77 @@
 package com.myleetcode.design.range_sum_query_2d_mutable;
 
-public class Solution {
-    class NumMatrix {
+class NumMatrix {
 
-        // Binary Indexed Tree: much better than Segment Tree to solve Range Query Problem
-        // https://en.wikipedia.org/wiki/Fenwick_tree
-        // https://www.hackerearth.com/zh/practice/data-structures/advanced-data-structures/fenwick-binary-indexed-trees/tutorial/
-        // https://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/
+    // Binary Indexed Tree: much better than Segment Tree to solve Range Query Problem
+    // https://en.wikipedia.org/wiki/Fenwick_tree
+    // https://www.hackerearth.com/zh/practice/data-structures/advanced-data-structures/fenwick-binary-indexed-trees/tutorial/
+    // https://www.geeksforgeeks.org/binary-indexed-tree-or-fenwick-tree-2/
 
-        // foundational concetp: how to represent a negative int to binary form?(normally 2's compliment)
+    // foundational concetp: how to represent a negative int to binary form?(normally 2's compliment)
 
-        // solution: https://leetcode.com/problems/range-sum-query-2d-mutable/discuss/75917/Share-my-Java-2-D-Binary-Indexed-Tree-Solution
+    // solution: https://leetcode.com/problems/range-sum-query-2d-mutable/discuss/75917/Share-my-Java-2-D-Binary-Indexed-Tree-Solution
 
-        int[][] matrix;
-        int[][] bITree;
+    int[][] matrix;
+    int[][] bITree;
 
-        // TC: O((R * C) * log(R * C))
-        // SC: O(R * C)
-        public NumMatrix(int[][] matrix) {
-            if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0){
-                return;
-            }
-
-            int rowLen = matrix.length;
-            int colLen = matrix[0].length;
-
-            this.matrix = new int[rowLen][colLen];
-            this.bITree = new int[rowLen + 1][colLen + 1]; // !!! for convience to implement, we should add one more row and col
-
-            // init
-            for(int i = 0; i < rowLen; i++){
-                for(int j = 0; j < colLen; j++){
-                    update(i, j, matrix[i][j]);
-                }
-            }
+    // TC: O((R * C) * log(R * C))
+    // SC: O(R * C)
+    public NumMatrix(int[][] matrix) {
+        if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0){
+            return;
         }
 
-        // TC: O(log(R * C))
-        public void update(int row, int col, int val) {
-            int rowLen = this.matrix.length;
-            int colLen = this.matrix[0].length;
+        int rowLen = matrix.length;
+        int colLen = matrix[0].length;
 
-            // get the delta of after update
-            int delta = val - this.matrix[row][col];
+        this.matrix = new int[rowLen][colLen];
+        this.bITree = new int[rowLen + 1][colLen + 1]; // !!! for convience to implement, we should add one more row and col
 
-            // update the matrix
-            this.matrix[row][col] = val;
-
-            // update the tree with the delta(diff), update the needed pos(row+1, col+1) and its children
-            // !!! be aware of the one more row and col in BIT
-            for(int i = row + 1; i <= rowLen; i += i & (-i)){
-                for(int j = col + 1; j <= colLen; j += j & (-j)){
-                    this.bITree[i][j] += delta;
-                }
+        // build the BITree
+        for(int i = 0; i < rowLen; i++){
+            for(int j = 0; j < colLen; j++){
+                update(i, j, matrix[i][j]);
             }
-        }
-
-        public int sumRegion(int row1, int col1, int row2, int col2) {
-            return sum(row2, col2) - sum(row2, col1 - 1) - sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
-        }
-
-        // TC: O(log(R * C))
-        // sum() returns the sum of leftcornor (0,0) to right cornor (row,col) elements in given array
-        private int sum(int row, int col){
-            int sum = 0;
-            // lookup the given node and all ancestors value
-            // !!! be aware of the one more row and col in BIT
-            for(int i = row + 1; i > 0; i -= i & (-i)){
-                for(int j = col + 1; j > 0; j -= j & (-j)){
-                    sum += this.bITree[i][j];
-                }
-            }
-            return sum;
         }
     }
+
+    // TC: O(log(R * C))
+    public void update(int arrRowIdx, int arrColIdx, int val) {
+        int rowLen = this.matrix.length;
+        int colLen = this.matrix[0].length;
+
+        // 1 get the diff to update the BITree
+        int diff = val - this.matrix[arrRowIdx][arrColIdx];
+
+        // 2 update the matrix
+        this.matrix[arrRowIdx][arrColIdx] = val;
+
+        // 3 update the BIT
+        for(int nodeRowIdx = arrRowIdx + 1; nodeRowIdx <= rowLen; nodeRowIdx += nodeRowIdx & (-nodeRowIdx)){
+            for(int nodeColIdx = arrColIdx + 1; nodeColIdx <= colLen; nodeColIdx += nodeColIdx & (-nodeColIdx)){
+                // update BITree
+                this.bITree[nodeRowIdx][nodeColIdx] += diff;
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum(row2, col2) - sum(row2, col1 - 1) - sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
+    }
+
+    // TC: O(log(R * C))
+    private int sum(int arrRowIdx, int arrColIdx){
+        int sum = 0;
+
+        for(int nodeRowIdx = arrRowIdx + 1; nodeRowIdx >= 1; nodeRowIdx -= nodeRowIdx & (-nodeRowIdx)){
+            for(int nodeColIdx = arrColIdx + 1; nodeColIdx >= 1; nodeColIdx -= nodeColIdx & (-nodeColIdx)){
+                sum += this.bITree[nodeRowIdx][nodeColIdx];
+            }
+        }
+
+        return sum;
+    }
+}
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
@@ -81,4 +79,3 @@ public class Solution {
  * obj.update(row,col,val);
  * int param_2 = obj.sumRegion(row1,col1,row2,col2);
  */
-}
