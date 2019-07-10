6774a74fb19a8e2c2ecaaf6c2606c6dd5a42c53e
Kenshin <hi_appler@126.com>
2019-07-10 16:48:12 -0700

    Linked List, Optimization: 21. Merge Two Sorted Lists

--- src/main/java/com/myleetcode/linked_list/merge_two_sorted_lists/MergeTwoSortedLists.java
+++ src/main/java/com/myleetcode/linked_list/merge_two_sorted_lists/MergeTwoSortedLists.java
@@ -1,21 +1,183 @@
 package com.myleetcode.linked_list.merge_two_sorted_lists;
 
-/**
- * 21. Merge Two Sorted Lists
-Easy
+public class MergeTwoSortedLists{
+    /**
+     * 21. Merge Two Sorted Lists
+     Easy
 
-1817
+     1817
 
-253
+     253
 
-Favorite
+     Favorite
 
-Share
-Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+     Share
+     Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
 
-Example:
+     Example:
 
-Input: 1->2->4, 1->3->4
-Output: 1->1->2->3->4->4
+     Input: 1->2->4, 1->3->4
+     Output: 1->1->2->3->4->4
 
- */
+     */
+
+    /**
+     * Linked List
+     */
+
+    /**
+     * Amazon
+     * |
+     * 38
+     *
+     * Adobe
+     * |
+     * 15
+     *
+     * Apple
+     * |
+     * 11
+     *
+     * Microsoft
+     * |
+     * 11
+     *
+     * Facebook
+     * |
+     * 8
+     *
+     * Google
+     * |
+     * 8
+     *
+     * Indeed
+     * |
+     * 4
+     *
+     * Bloomberg
+     * |
+     * 4
+     *
+     * Yahoo
+     * |
+     * 3
+     *
+     * Arista Networks
+     * |
+     * 3
+     *
+     * ByteDance
+     * |
+     * 3
+     *
+     * Uber
+     * |
+     * 2
+     *
+     * VMware
+     * |
+     * 2
+     *
+     * Yandex
+     * |
+     * 2
+     */
+
+    /**
+     * 6m-1y
+     * Alibaba
+     * |
+     * 3
+     *
+     * Tencent
+     * |
+     * 3
+     *
+     * Walmart Labs
+     * |
+     * 3
+     *
+     * Oracle
+     * |
+     * 3
+     *
+     * Cisco
+     * |
+     * 3
+     *
+     * Airbnb
+     * |
+     * 2
+     *
+     * IBM
+     * |
+     * 2
+     *
+     * Huawei
+     * |
+     * 2
+     *
+     * Paypal
+     * |
+     * 2
+     *
+     * Visa
+     * |
+     * 2
+     */
+
+    /**
+     * 1y-2y
+     * Yelp
+     * |
+     * 5
+     *
+     * Baidu
+     * |
+     * 5
+     *
+     * LinkedIn
+     * |
+     * 2
+     *
+     * Coursera
+     * |
+     * 2
+     *
+     * EMC
+     * |
+     * 2
+     *
+     * Hulu
+     * |
+     * 2
+     *
+     * Intuit
+     * |
+     * 2
+     *
+     * Expedia
+     * |
+     * 2
+     *
+     * Groupon
+     * |
+     * 2
+     *
+     * Capital One
+     * |
+     * 2
+     *
+     * Meituan
+     * |
+     * 2
+     *
+     * Evernote
+     * |
+     * 2
+     *
+     * Reddit
+     * |
+     * 2
+     */
+}
--- src/main/java/com/myleetcode/linked_list/merge_two_sorted_lists/Solution.java
+++ src/main/java/com/myleetcode/linked_list/merge_two_sorted_lists/Solution.java
@@ -1,5 +1,7 @@
 package com.myleetcode.linked_list.merge_two_sorted_lists;
 
+import com.myleetcode.utils.list_node.ListNode;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -10,51 +12,50 @@ package com.myleetcode.linked_list.merge_two_sorted_lists;
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-        // 直观
         return mergeTwoListsByIterative(l1, l2);
-        
-        // 不好想
+
         // return mergeByRecursive(l1, l2);
         
     }
-    
-    private ListNode mergeTwoListsByIterative(ListNode l1, ListNode l2){
-        // 本质是构建一个新链表。
-//         通用方式为：一个dummyNode，一个用来记录 当前要判断的两个node被选出的node 要被附着到的node，也就是prevNode；l1和l2都不为null的情况下，比较l1z指向的node和l2指向的node的val，小的那一个，被prevNode的next指向。这时，当前的prevNode的next更新好了，接下来很重要，我们要更新prevNode本身，因为我们已经链接了一个新的node，那么这个新node就是prevNode了，更新。然后小的那一个后移；最后，未完者附着结尾；最后，返回dummyNode.next
-        
-        ListNode dummyNode = new ListNode(0);
-        ListNode prevNode = dummyNode;
-        
-        while(l1 != null && l2 != null){
-            if(l1.val <= l2.val){
-                // 更新prevNode.next和prevNode自身的顺序是严格的，不可互换.
-                // 当前prevNode的下一个node应该是l1
-                prevNode.next = l1;
-                // 更新当前的prevNode
-                prevNode = l1;
-                // l1后移动
-                l1 = l1.next;
+
+    // TC: O(N), N is the length of the LL
+    // SC: O(1)
+    private ListNode mergeTwoListsByIterative(ListNode firstHead, ListNode secondHead){
+        if(firstHead == null){
+            return secondHead;
+        }
+        if(secondHead == null){
+            return firstHead;
+        }
+
+        ListNode preHead = new ListNode(-1);
+        ListNode curNode = preHead;
+        while(firstHead != null && secondHead != null){
+            if(firstHead.val >= secondHead.val){
+                curNode.next = secondHead;
+
+                secondHead = secondHead.next;
             }else{
-                // 同理
-                prevNode.next = l2;
-                prevNode  = l2;
-                l2 = l2.next;
+                curNode.next = firstHead;
+
+                firstHead = firstHead.next;
             }
+
+            curNode = curNode.next;
         }
-        
-        if(l1 == null && l2 != null){
-            prevNode.next = l2;
+        if(firstHead != null){
+            curNode.next = firstHead;
         }
-        if(l2 == null && l1 != null){
-            prevNode.next = l1;
+        if(secondHead != null){
+            curNode.next = secondHead;
         }
-        
-        return dummyNode.next;
-        
+
+        return preHead.next;
     }
     
-    // 另外有一个recursive的方法，不是很容易想通
+    // 另外有一个recursive的方法
+    // TC: O(N)
+    // SC: O(N)
     private ListNode mergeByRecursive(ListNode l1, ListNode l2){
         /*The base cases are if l1 or l2 is null, return the other ListNode. This also holds for the case when l1==l2==null.
 For the recursive case, if the value of l1 is less than that of l2, l1 should be ahead of l2 in the returned list. That's why we return ListNode l1 in this case. We also change the next value of l1 to keep merging. For instance: l1=1->2 ; l2=2->3. First step, l1<l2 (1<2), so the first value in the list should be l1=1. Then merge the rest of the first list (2) and the second list (2->3) to find the remaining of the list.
