2067ef709f9c04f01ccd1713f7ab4f8d1f475f48
Kenshin <hi_appler@126.com>
2019-07-17 19:05:21 -0700

    Sliding Window, Binary Search: 209. Minimum Size Subarray Sum

---
+++ src/main/java/com/myleetcode/sliding_window/minimum_size_subarray_sum/MinimumSizeSubarraySum.java
@@ -0,0 +1,63 @@
+package com.myleetcode.sliding_window.minimum_size_subarray_sum;
+
+public class MinimumSizeSubarraySum {
+    /**
+     * 209. Minimum Size Subarray Sum
+     * Medium
+     *
+     * 1197
+     *
+     * 77
+     *
+     * Favorite
+     *
+     * Share
+     * Given an array of n positive integers and a positive integer s, find the minimal length of a contiguous subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+     *
+     * Example:
+     *
+     * Input: s = 7, nums = [2,3,1,2,4,3]
+     * Output: 2
+     * Explanation: the subarray [4,3] has the minimal length under the problem constraint.
+     * Follow up:
+     * If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
+     */
+
+    /**
+     * Array
+     * Two Pointers
+     * Binary Search
+     */
+
+    /**
+     * Goldman Sachs
+     * |
+     * 13
+     *
+     * Facebook
+     * |
+     * 2
+     *
+     * Microsoft
+     * |
+     * 2
+     *
+     * 6m-1y
+     * Bloomberg
+     * |
+     * 2
+     *
+     * Google
+     * |
+     * 2
+     *
+     * 1y-2y
+     * Salesforce
+     * |
+     * 2
+     *
+     * Amazon
+     * |
+     * 2
+     */
+}
