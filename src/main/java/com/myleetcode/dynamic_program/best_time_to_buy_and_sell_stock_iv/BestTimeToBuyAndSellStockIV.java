db92882acb40694e5b6608746fe2875205a25aa5
Kenshin <hi_appler@126.com>
2019-07-29 15:38:21 -0700

    DP: 188. Best Time to Buy and Sell Stock IV

---
+++ src/main/java/com/myleetcode/dynamic_program/best_time_to_buy_and_sell_stock_iv/BestTimeToBuyAndSellStockIV.java
@@ -0,0 +1,61 @@
+package com.myleetcode.dynamic_program.best_time_to_buy_and_sell_stock_iv;
+
+public class BestTimeToBuyAndSellStockIV {
+    /**
+     * 188. Best Time to Buy and Sell Stock IV
+     * Hard
+     *
+     * 824
+     *
+     * 54
+     *
+     * Favorite
+     *
+     * Share
+     * Say you have an array for which the ith element is the price of a given stock on day i.
+     *
+     * Design an algorithm to find the maximum profit. You may complete at most k transactions.
+     *
+     * Note:
+     * You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+     *
+     * Example 1:
+     *
+     * Input: [2,4,1], k = 2
+     * Output: 2
+     * Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+     * Example 2:
+     *
+     * Input: [3,2,6,5,0,3], k = 2
+     * Output: 7
+     * Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4.
+     *              Then buy on day 5 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+     */
+
+    /**
+     * Dynamic Programming
+     */
+
+    /**
+     * Amazon
+     * |
+     * 6
+     *
+     * Google
+     * |
+     * 3
+     *
+     * Facebook
+     * |
+     * 3
+     *
+     * Two Sigma
+     * |
+     * 2
+     *
+     * 1-2y
+     * Citadel
+     * |
+     * 3
+     */
+}
