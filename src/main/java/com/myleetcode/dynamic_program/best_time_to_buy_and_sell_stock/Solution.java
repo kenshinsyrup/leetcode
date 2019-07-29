0ee88c769110d34e464921816ebf8cbcb5e9ab2a
Kenshin <hi_appler@126.com>
2019-07-29 15:32:36 -0700

    DP, Optimization: 121. Best Time to Buy and Sell Stock

--- src/main/java/com/myleetcode/dynamic_program/best_time_to_buy_and_sell_stock/Solution.java
+++ src/main/java/com/myleetcode/dynamic_program/best_time_to_buy_and_sell_stock/Solution.java
@@ -1,51 +1,37 @@
 package com.myleetcode.dynamic_program.best_time_to_buy_and_sell_stock;
 
-public class Solution {
+class Solution {
     public int maxProfit(int[] prices) {
         return maxProfiltByDP(prices);
     }
 
+    /*
+    出错点：
+    1 dp问题确定
+    2 currentMaxProfit来源为prices[i-1]-minPrice, 这里我们需要一个prices[i-1]之前的minPrice，所以我们用一个变量来记录和更新他
+    */
+
+    // dp problem, because it could be reduced ot sub-problem and with the ans of sub-problem we could get the ans of the problem
+    // dp[i] means given first i elems in the prices array, ie prices[0:i-1], the best profit we could get.
+    // dp[i] = max(dp[i-1], currentMaxProfit), the currentMaxProfit is price[i - 1]-minPrice, where minPrice is the lowest price before prices[i-1].
     // TC: O(N)
     // SC: O(N)
     // intuition: DP. 52, 121
-    // dp[i] means the max profit at ith day, then dp[i] = max(dp[i-1], prices[i] - min(prices[0:i-1]))
-    // base is the dp[0], because we are only have the 0th day, then profit must be 0. and the min price is prices[0]. so we have no need to have extra row as base
     private int maxProfiltByDP(int[] prices){
         if(prices == null || prices.length == 0){
             return 0;
         }
 
         int len = prices.length;
-        // dp
-        int[] dp = new int[len];// initial profit are all 0, the is base
 
-        // min price at first is prices[0]
-        int minPrice = prices[0];
+        int[] dp = new int[len + 1];
+        int minPrice = Integer.MAX_VALUE;
+        for(int i = 1; i <= len; i++){
+            dp[i] = Math.max(dp[i - 1], prices[i - 1] - minPrice);
 
-        // dp
-        for(int i = 1; i < len; i++){
-            dp[i] = Math.max(dp[i - 1], prices[i] - minPrice);
-
-            // update minPrice
-            minPrice = Math.min(minPrice, prices[i]);
+            minPrice = Math.min(minPrice, prices[i - 1]);
         }
 
-        return dp[len - 1];
+        return dp[len];
     }
-    
-    // 严格套用Kadane's Algorithm的写法：
-    //*maxCur = current maximum value
-// *maxSoFar = maximum value found so far
-    private int maxProfitByKadane(int[] prices){
-    int maxCur = 0, maxSoFar = 0, min = prices[0];
-        
-    /*begin with 1 because we already set the first element to min, no need to run this redundant loop; of course you can begin with 0 too, but it slows the runtime by 1ms*/
-    for(int i = 1; i < prices.length; i++){
-        //check the difference of each element and the current min, update min if necessary.
-        maxCur = prices[i] - ( (prices[i] < min) ? (min = prices[i]) : (min = min) );
-        maxSoFar = Math.max(maxSoFar, maxCur); 
-     }
-        
-    return maxSoFar;
-}
 }
