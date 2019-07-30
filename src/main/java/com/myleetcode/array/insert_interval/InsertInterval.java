9814a7921f7402216ac173a8ddc8b9621a4bf098
Kenshin <hi_appler@126.com>
2019-07-30 12:30:03 -0700

    Array, Sort: 57. Insert Interval

---
+++ src/main/java/com/myleetcode/array/insert_interval/InsertInterval.java
@@ -0,0 +1,83 @@
+package com.myleetcode.array.insert_interval;
+
+public class InsertInterval {
+    /**
+     * 57. Insert Interval
+     * Hard
+     *
+     * 910
+     *
+     * 118
+     *
+     * Favorite
+     *
+     * Share
+     * Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+     *
+     * You may assume that the intervals were initially sorted according to their start times.
+     *
+     * Example 1:
+     *
+     * Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+     * Output: [[1,5],[6,9]]
+     * Example 2:
+     *
+     * Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+     * Output: [[1,2],[3,10],[12,16]]
+     * Explanation: Because the new interval [4,8] overlaps with [3,5],[6,7],[8,10].
+     * NOTE: input types have been changed on April 15, 2019. Please reset to default code definition to get new method signature.
+     */
+
+    /**
+     * Array
+     * Sort
+     */
+
+    /**
+     * Google
+     * |
+     * 10
+     *
+     * Amazon
+     * |
+     * 8
+     *
+     * Uber
+     * |
+     * 4
+     *
+     * LinkedIn
+     * |
+     * 4
+     *
+     * Microsoft
+     * |
+     * 3
+     *
+     * Facebook
+     * |
+     * 2
+     *
+     * Twitter
+     * |
+     * 2
+     *
+     * Apple
+     * |
+     * 2
+     *
+     * 6m-1y
+     * Tableau
+     * |
+     * 2
+     *
+     * 1y-2y
+     * Yahoo
+     * |
+     * 2
+     *
+     * Snapchat
+     * |
+     * 2
+     */
+}
