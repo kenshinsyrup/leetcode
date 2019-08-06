0ea4b24c0e53f081d41ef985d47e5d629459a110
Kenshin <hi_appler@126.com>
2019-08-05 22:46:52 -0700

    DP: 410. Split Array Largest Sum

---
+++ src/main/java/com/myleetcode/dynamic_program/split_array_largest_sum/Solution.java
@@ -0,0 +1,4 @@
+package com.myleetcode.dynamic_program.split_array_largest_sum;
+
+public class Solution {
+}
---
+++ src/main/java/com/myleetcode/dynamic_program/split_array_largest_sum/SplitArrayLargestSum.java
@@ -0,0 +1,67 @@
+package com.myleetcode.dynamic_program.split_array_largest_sum;
+
+public class SplitArrayLargestSum {
+    /**
+     * 410. Split Array Largest Sum
+     * Hard
+     *
+     * 822
+     *
+     * 45
+     *
+     * Favorite
+     *
+     * Share
+     * Given an array which consists of non-negative integers and an integer m, you can split the array into m non-empty continuous subarrays. Write an algorithm to minimize the largest sum among these m subarrays.
+     *
+     * Note:
+     * If n is the length of array, assume the following constraints are satisfied:
+     *
+     * 1 ≤ n ≤ 1000
+     * 1 ≤ m ≤ min(50, n)
+     * Examples:
+     *
+     * Input:
+     * nums = [7,2,5,10,8]
+     * m = 2
+     *
+     * Output:
+     * 18
+     *
+     * Explanation:
+     * There are four ways to split nums into two subarrays.
+     * The best way is to split it into [7,2,5] and [10,8],
+     * where the largest sum among the two subarrays is only 18.
+     */
+
+    /**
+     * Dynamic Programming
+     */
+
+    /**
+     * Google
+     * |
+     * 37
+     *
+     * 1y-2y
+     * Airbnb
+     * |
+     * 4
+     *
+     * Bloomberg
+     * |
+     * 2
+     *
+     * Amazon
+     * |
+     * 2
+     *
+     * Facebook
+     * |
+     * LeetCode
+     *
+     * Baidu
+     * |
+     * LeetCode
+     */
+}
