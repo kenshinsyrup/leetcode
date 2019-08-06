eaf969addb64253237cc6b13917975a74f8a7b52
Kenshin <hi_appler@126.com>
2019-08-05 18:34:39 -0700

    Greedy: 452. Minimum Number of Arrows to Burst Balloons

--- src/main/java/com/myleetcode/greedy/minimum_number_of_arrows_to_burst_balloons/Solution.java
+++ src/main/java/com/myleetcode/greedy/minimum_number_of_arrows_to_burst_balloons/Solution.java
@@ -1,25 +1,63 @@
 package com.myleetcode.greedy.minimum_number_of_arrows_to_burst_balloons;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 
 class Solution {
     public int findMinArrowShots(int[][] points) {
+        // return findMinArrowShotsBySort(points); // optimization
+        return findMinArrowShotsByMerge(points);
+    }
 
+    // same like Merge Interval but to make it min
+    // TC: O(N)
+    // SC: O(N)
+    private int findMinArrowShotsByMerge(int[][] points){
         // special case
         if(points == null || points.length == 0){
             return 0;
         }
 
-        return findMinArrowShotsBySort(points);
+        // Sort, start ascending
+        Arrays.sort(points, new Comparator<int[]>(){
+            public int compare(int[] p1, int[] p2){
+                return p1[0] - p2[0];
+            }
+        });
 
+        int len = points.length;
+        List<int[]> ret = new ArrayList<>();
+        ret.add(points[0]);
+        for(int i = 0; i < len; i++){
+            int[] point = points[i];
+
+            int[] retEndPoint = ret.get(ret.size() - 1);
+            if(retEndPoint[1] >= point[0]){
+                retEndPoint[0] = Math.min(retEndPoint[0], point[0]);
+                retEndPoint[1] = Math.min(retEndPoint[1], point[1]);
+            }else{
+                ret.add(point);
+            }
+        }
+
+        return ret.size();
     }
 
+    // Optimization: reduce the SC
+    // 画一下图还是很有帮助的对这道题
+    // intuition: first we sort all points by start, then we check for overlaps
+    // if find overlaps between cur and next, we know we could use one arrow to burst these two, and then, we need to remember the min value of ends, and keep looking for next overlap with this end.
+    // if no overlap foud with cur end, meaning we should shoot another arrow, so we update the new end and back to our starting.
+    // same like Merge Interval but to make it min
+    // TC: O(N)
+    // SC: O(1)
     private int findMinArrowShotsBySort(int[][] points){
-        // 画一下图还是很有帮助的对这道题
-        // intuition: first we sort all points by start, then we check for overlaps
-        // if find overlaps between cur and next, we know we could use one arrow to burst these two, and then, we need to remember the min value of ends, and keep looking for next overlap with this end.
-        // if no overlap foud with cur end, meaning we should shoot another arrow, so we update the new end and back to our starting.
+        // special case
+        if(points == null || points.length == 0){
+            return 0;
+        }
 
         // Sort, start ascending
         Arrays.sort(points, new Comparator<int[]>(){
