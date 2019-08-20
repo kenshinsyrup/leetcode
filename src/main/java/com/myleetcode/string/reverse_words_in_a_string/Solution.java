ad3cb41be0412ccefde015db372ff088022aecc2
Kenshin <hi_appler@126.com>
2019-08-20 16:24:59 -0700

    String, Solid Code: 151. Reverse Words in a String

--- src/main/java/com/myleetcode/string/reverse_words_in_a_string/Solution.java
+++ src/main/java/com/myleetcode/string/reverse_words_in_a_string/Solution.java
@@ -2,11 +2,72 @@ package com.myleetcode.string.reverse_words_in_a_string;
 
 class Solution {
     public String reverseWords(String s) {
-        return reverseWordsByTwoPointers(s);
+        // return reverseWordsByTwoPointers(s);
+        return reverseWordsByReverse(s);
+    }
+
+    // use this solution
+    // use the same sol with 186. Reverse Words in a String II
+    // TC: O(N)
+    // SC: O(N)
+    private String reverseWordsByReverse(String str){
+        if(str == null || str.length() == 0){
+            return str;
+        }
+
+        str = str.trim(); // remove leading and tailing spaces
+        int len = str.length();
+        char[] charArray = str.toCharArray();
+
+        // 1 reverse the whole string
+        reverse(charArray, 0, len - 1);
+
+        // 2 reverse every word
+        int i = 0;
+        int j = 0;
+        while(j < len){
+            // find the delimiter, reverse the word str[i:j-1]
+            while(j < len && charArray[j] != ' '){
+                j++;
+            }
+            reverse(charArray, i, j - 1);
+
+            // skip spaces
+            while(j < len && charArray[j] == ' '){
+                j++;
+            }
+
+            // move i
+            i = j;
+        }
+
+        // 3 build ans string, remove useless spaces
+        StringBuilder sb = new StringBuilder();
+        for(int idx = 0; idx < len; idx++){
+            // skip useless spaces
+            if(sb.length() > 0 && sb.charAt(sb.length() - 1) == ' ' && charArray[idx] == ' '){
+                continue;
+            }
+
+            sb.append(charArray[idx]);
+        }
+
+        return sb.toString();
+    }
+
+    private void reverse(char[] charArray, int start, int end){
+        while(start < end){
+            char temp = charArray[start];
+            charArray[start] = charArray[end];
+            charArray[end] = temp;
+
+            start++;
+            end--;
+        }
     }
 
     // TC: O(N)
-    // SC: O(1)
+    // SC: O(N)
     // intuition: String Reverse
     // for the requirment: "Your reversed string should not contain leading or trailing spaces." and "You need to reduce multiple spaces between two words to a single space in the reversed string." , we could accomplish this automatically if we extracts words in String and concatnate them reversely
     // there's are a lot of approaches, the most classic two: 1 convert String to words List and concatnate them in reverse order, this cost additional O(N) SC. 2 use Two Pointers, startP and endP, from the String's tail to traverse the String, when startP find a non space char, endP moves to startP, this is the end of current word, then startP moves to find the head of current word, then we get a word, repeat this
