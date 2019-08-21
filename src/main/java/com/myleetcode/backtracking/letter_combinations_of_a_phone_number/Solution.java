42f93394e4a19d1f5fdb16648cf490064dd187a3
Kenshin <hi_appler@126.com>
2019-08-20 22:21:38 -0700

    Backtracking: 17. Letter Combinations of a Phone Number

--- src/main/java/com/myleetcode/backtracking/letter_combinations_of_a_phone_number/Solution.java
+++ src/main/java/com/myleetcode/backtracking/letter_combinations_of_a_phone_number/Solution.java
@@ -5,53 +5,75 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Solution {
+class Solution {
     public List<String> letterCombinations(String digits) {
-        // first need a map like {2:[a, b, c], 3:[d, e, f]...}
-        // traverse all keys and build combination
 
-        List<String> ans = new ArrayList<String>();
-
-        // special case
-        if(digits == null || digits.length() == 0){
-            return ans;
-        }
-
-        // construct num -> letter map
-        Map<String, String> numLetters = new HashMap<String, String>();
-        numLetters.put("2", "abc");
-        numLetters.put("3", "def");
-        numLetters.put("4", "ghi");
-        numLetters.put("5", "jkl");
-        numLetters.put("6", "mno");
-        numLetters.put("7", "pqrs");
-        numLetters.put("8", "tuv");
-        numLetters.put("9", "wxyz");
-
-        letterCombinationsByBacktracking(ans, numLetters, digits, "");
-
-        return ans;
+        return letterCombinationsByBacktracking(digits);
 
     }
 
-    private void letterCombinationsByBacktracking(List<String> ans, Map<String, String> numLetters, String digits, String temp){
-        // 加入ans的条件
-        if(digits.length() == 0){
-            ans.add(temp);
-        }else{
-            // 用掉第一个digit
-            String digit = digits.substring(0, 1);
-//             找到这个digit对应的letters
-            String letters = numLetters.get(digit);
-            for(int i = 0; i < letters.length(); i++){
-                // 遍历当前num对应的letters，每次取一个letter
-                String letter = letters.substring(i, i + 1);
-//                 temp加入当前letter，向下深入时减去digits已经用过的一个digit
-                temp += letter;
-                letterCombinationsByBacktracking(ans, numLetters, digits.substring(1), temp);
-//                 temp恢复
-                temp = temp.substring(0, temp.length() - 1);
-            }
+    // intuition: Backtracking
+    // first need a map like {2:[a, b, c], 3:[d, e, f]...}
+    // traverse all keys and build combination
+    /*
+    if make it simple, we could say:
+    TC: O(2^N)
+    SC: O(2^N).
+
+    but to be strict, should know:
+    TC: O(3^N * 4^M), where N is the number of digits in the input that maps to 3 letters (e.g. 2, 3, 4, 5, 6, 8) and M is the number of digits in the input that maps to 4 letters (e.g. 7, 9), and N+M is the total number digits in the input.
+
+    SC: O(3^N * 4^M), since one has to keep O(3^N * 4^M) solutions.
+    */
+    private List<String> letterCombinationsByBacktracking(String digits){
+        List<String> ret = new ArrayList<String>();
+
+        // special case
+        if(digits == null || digits.length() == 0){
+            return ret;
+        }
+
+        // construct num -> letter map
+        Map<String, String> numLettersMap = new HashMap<String, String>();
+        numLettersMap.put("2", "abc");
+        numLettersMap.put("3", "def");
+        numLettersMap.put("4", "ghi");
+        numLettersMap.put("5", "jkl");
+        numLettersMap.put("6", "mno");
+        numLettersMap.put("7", "pqrs");
+        numLettersMap.put("8", "tuv");
+        numLettersMap.put("9", "wxyz");
+
+        backtracking(ret, numLettersMap, digits, 0, new StringBuilder());
+
+        return ret;
+    }
+
+    private void backtracking(List<String> ret, Map<String, String> numLettersMap, String digits, int idx, StringBuilder sb){
+        // 1. base, digits exhausted
+        if(idx == digits.length()){
+            ret.add(sb.toString());
+            return;
+        }
+
+        // 2. try the letters according to current num
+        // 2.1. get current letters string
+        String curNumStr = digits.substring(idx, idx + 1);
+        String curLettersStr = numLettersMap.get(curNumStr);
+        if(curLettersStr == null){
+            return;
+        }
+
+        // 2.2 try one char in the letters string
+        for(int i = 0; i < curLettersStr.length(); i++){
+            // append ith letter
+            sb.append(curLettersStr.substring(i, i + 1));
+
+            // explore next idx
+            backtracking(ret, numLettersMap, digits, idx + 1, sb);
+
+            // backtrack
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
