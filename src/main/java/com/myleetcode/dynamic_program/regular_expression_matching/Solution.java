70b8f166ca568ea0dd2d64d362b612ce4435fe83
Kenshin <hi_appler@126.com>
2019-08-21 14:15:29 -0700

    DP: 10. Regular Expression Matching

--- src/main/java/com/myleetcode/dynamic_program/regular_expression_matching/Solution.java
+++ src/main/java/com/myleetcode/dynamic_program/regular_expression_matching/Solution.java
@@ -10,8 +10,22 @@ class Solution {
     // 1 dp[i][j] = dp[i-1][j-1], if S[i-1] == P[j-1]
     // 2 dp[i][j] = dp[i-1][j-1], if P[j-1] == '.'
     // 3 if P[j-1] == '*', then
-    // 3.1 if S[i-1] != P[j-2], dp[i][j] = dp[i-1][j-2]
-    // 3.2 if S[i-1] == P[j-2], dp[i][j] = or(dp[i-1][j], dp[i][j-1], dp[i-1][j-2]), dp[i-1][j] means * as multiple, dp[i][j-1] means * as single char, dp[i-1][j-2] means * as 0 char, we choose any of true status from this
+    // 3.1 if S[i-1] != P[j-2], dp[i][j] = dp[i-1][j-2], *代表匹配0次
+    // 3.2 if S[i-1] == P[j-2], dp[i][j] = or(dp[i-1][j], dp[i-1][j-2]), dp[i-1][j] means * as multiple, dp[i-1][j-2] means * as 0 char, we choose any of true status from this. 只需要分为两个情况，* 用作匹配0次或者大于0次，0次我们知道了表达式；对于大于0次，实际就是匹配完当前s的char，继续用她去匹配其他(之后仍然是0次或者多于0次)
+    /*
+    https://leetcode.com/problems/regular-expression-matching/discuss/333806/topic
+
+当s[i]==p[j]或者p[j]=='.'时，目标串和模式串的最后两个字符匹配，那么原问题转移为i-1，j-1大小的子问题,ismatch[i][j]=ismatch[i-1][j-1]
+当p[j]=='*'时，'*'可以匹配前面一个字符任意多次，可以分成两类：匹配0次或大于0次，最后的结果是这两种情况只要有一个为true则为true
+当匹配0次时，ismatch[i][j]=ismatch[i][j-2]
+当匹配大于0次时，有一种递归的思想在里面，先匹配1次，然后再递归地匹配0次或者大于0次
+我举例子说明
+s=baa
+p=ba*
+当比较到第3个字符时，匹配大于0次，那么先匹配1次，后面还能再匹配0次或大于0次，p变成baa*
+因为s的第3个字符和p的第3个字符是相等的，那么可以同时去掉这两个字符，s=ba，p=ba*再比较这两个字符串。这种情况原问题ismatch[i][j]转移成了ismatch[i-1][j]的子问题，条件是s[i]==p[j-1]||p[j-1]=='.'
+最后，初始情况是s的大小为空，模式串的大小变化，就只有p[j]=='*'且匹配0次的情况。
+    */
     // https://leetcode.com/problems/regular-expression-matching/discuss/161365/Java-solution-with-more-detailed-explanation
     private boolean isMatchByDP(String s, String p){
         if(s == null || p == null){
@@ -22,8 +36,6 @@ class Solution {
         int pLen = p.length();
 
         boolean[][] dp = new boolean[sLen + 1][pLen + 1];
-
-        // dp中有一个重点是坐标的转换，比较容易懵逼。比如下面代码里，for循环条件里的i和j从1到len，是给dp用的坐标，表达的是：1based的系统下，第i个位置的char和第j个位置的char的匹配关系，存入dp[i][j]。但是由于charAt方法是0based的，所以所有charAt方法处取char时，都比正常的 取第几个char 这个操作多减去了一个1.其余如思路链接所示。
 
         // base case
         dp[0][0] = true;
@@ -40,7 +52,7 @@ class Solution {
                     dp[i][j] = dp[i - 1][j - 1];
                 }else if(p.charAt(j - 1) == '*'){
                     if(p.charAt(j - 2) == s.charAt(i - 1) || p.charAt(j - 2) == '.'){
-                        //  *对应0个s中的char        *对应多个s中的char
+                        //  *对应0个s中的char     *对应多个s中的char
                         dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
                     }else{
                         // *必须当做0
