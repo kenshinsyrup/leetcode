0566857fccbe9871b5f588abca5ac82265c4ace6
Kenshin <hi_appler@126.com>
2019-08-26 19:17:17 -0700

    DP: 85. Maximal Rectangle

--- src/main/java/com/myleetcode/dynamic_program/maximal_rectangle/Solution.java
+++ src/main/java/com/myleetcode/dynamic_program/maximal_rectangle/Solution.java
@@ -9,31 +9,45 @@ class Solution {
     // SC: O(R*C)
     // intuition: DP. 85, 221, 304
     // according to the solution, we could use the dp with histogram to solve this.
+    /*
+    dp[i][j] represents the width(num of 1s) from last 0(or 0th idx) at ith row and jth col
+
+    dp[i][j] = dp[i][j - 1] + 1, if matrix[i][j] is 1
+
+    base case:
+    dp default is 0
+    dp[0][0] = 1, if matrix[0][0] is 1
+    for the 0th col, dp[i][0] = 1, if matrix[i][0] is 1
+    for the 0th row, dp[0][j] = dp[0][j - 1] + 1, if matrix[0][j] is 1
+    */
     private int maximalRectangleByDP(char[][] matrix){
         if(matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0){
             return 0;
         }
 
-        // dp[i][j] represents the max width at ith row, 0:jth cols, then the area have relations with the dp
         int rows = matrix.length;
         int cols = matrix[0].length;
         int[][] dp = new int[rows][cols];
-        // base
-        for(int i = 0; i < rows; i++){
+
+        // 1. base
+        // dp[0][0]
+        if(matrix[0][0] == '1'){
+            dp[0][0] = 1;
+        }
+        // dp[i][0]
+        for(int i = 1; i < rows; i++){
             if(matrix[i][0] == '1'){
                 dp[i][0] = 1;
             }
         }
-        for(int j = 0; j < cols; j++){
+        // dp[0][j]
+        for(int j = 1; j < cols; j++){
             if(matrix[0][j] == '1'){
-                if(j == 0){
-                    dp[0][j] = 1;
-                }else{
-                    dp[0][j] = 1 + dp[0][j-1];
-                }
+                dp[0][j] = 1 + dp[0][j-1];
             }
         }
 
+        // 2. explore
         for(int i = 1; i < rows; i++){
             for(int j = 1; j < cols; j++){
                 if(matrix[i][j] == '1'){
@@ -43,12 +57,16 @@ class Solution {
         }
 
         int maxArea = 0;
-        // since we have get all the histograms, we could caculate the areas and choose the max one
+        // 3. since we have get all the width for every given i,j
+        // we could caculate the areas and choose the max one by try every histogram with given i,j as rightbottom cell
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
-                // min width in j col, must keep it out of k loop to record the min
-                int width = dp[i][j];
+                // !!! try every cell(i,j) as the rightbotoom of histogram, width is dp[i][j]
+                // if this cell(i,j) is 1, we know cell(i,j) is the rightbottom of a valid all 1s histogram
                 if(matrix[i][j] == '1'){
+                    int width = dp[i][j];
+
+                    // !!! now we try to increase the height the histogram
                     for(int k = i; k >= 0; k--){
                         // keep updating the min width
                         width = Math.min(width, dp[k][j]);
