dcc600b46f1b3ca40071fbff891b6f53c65987e6
Kenshin <hi_appler@126.com>
2019-09-10 15:46:28 -0700

    LinkedList, Solid Code: 2. Add Two Numbers

--- src/main/java/com/myleetcode/linked_list/addtwonumbers/AddTwoNumbers.java
+++ src/main/java/com/myleetcode/linked_list/addtwonumbers/AddTwoNumbers.java
@@ -1,20 +1,185 @@
 package com.myleetcode.linked_list.addtwonumbers;
 
-/*
-You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+public class AddTwoNumbers{
+/**
+ *2. Add Two Numbers
+ * Medium
+ *
+ * 5917
+ *
+ * 1537
+ *
+ * Favorite
+ *
+ * Share
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ */
 
-You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+/**
+ * Linked List
+ * Math
+ *
+ * Solid Code
+ */
 
-Example:
-
-Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-Output: 7 -> 0 -> 8
-Explanation: 342 + 465 = 807.
-*/
-public class AddTwoNumbers {
-    public static void main(String[] args) {
-        System.out.println("add two numbers.");
-        // Solution s = new Solution();
-        // System.out.println(s);
-    }
+/**
+ * Amazon
+ * |
+ * 47
+ *
+ * Google
+ * |
+ * 18
+ *
+ * Adobe
+ * |
+ * 18
+ *
+ * Microsoft
+ * |
+ * 15
+ *
+ * Apple
+ * |
+ * 14
+ *
+ * Bloomberg
+ * |
+ * 8
+ *
+ * Facebook
+ * |
+ * 5
+ *
+ * Cisco
+ * |
+ * 4
+ *
+ * Uber
+ * |
+ * 4
+ *
+ * Yandex
+ * |
+ * 4
+ *
+ * Oracle
+ * |
+ * 2
+ *
+ * ByteDance
+ * |
+ * 2
+ *
+ * Yahoo
+ * |
+ * 2
+ *
+ * Grab
+ * |
+ * 2
+ *
+ * 6m-1y
+ * Baidu
+ * |
+ * 3
+ *
+ * Mathworks
+ * |
+ * 3
+ *
+ * Alibaba
+ * |
+ * 3
+ *
+ * Aetion
+ * |
+ * 3
+ *
+ * IBM
+ * |
+ * 3
+ *
+ * VMware
+ * |
+ * 2
+ *
+ * Nvidia
+ * |
+ * 2
+ *
+ * Intel
+ * |
+ * 2
+ *
+ * Capital One
+ * |
+ * 2
+ *
+ * Flipkart
+ * |
+ * 2
+ *
+ * Wish
+ * |
+ * 2
+ *
+ * Redfin
+ * |
+ * 2
+ *
+ * Zoho
+ * |
+ * 2
+ *
+ * 1y-2y
+ * Airbnb
+ * |
+ * 9
+ *
+ * Tencent
+ * |
+ * 6
+ *
+ * GoDaddy
+ * |
+ * 3
+ *
+ * Lyft
+ * |
+ * 3
+ *
+ * LinkedIn
+ * |
+ * 3
+ *
+ * Gilt Groupe
+ * |
+ * 2
+ *
+ * Zappos
+ * |
+ * 2
+ *
+ * NetEase
+ * |
+ * 2
+ *
+ * AppDynamics
+ * |
+ * 2
+ *
+ * SAP
+ * |
+ * 2
+ */
 }
+
--- src/main/java/com/myleetcode/linked_list/addtwonumbers/Solution.java
+++ src/main/java/com/myleetcode/linked_list/addtwonumbers/Solution.java
@@ -13,129 +13,80 @@ import com.myleetcode.utils.list_node.ListNode;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        // 最初的思路是先翻转链表，然后按位求和，链表长度不一时注意检查长链表的剩余部分，注意检查最后的进位位是否大于0
-        // 注意：实际上是不需要先翻转链表的，因为给的输入链表已经是和自然数反序的了，即从左到右是从低位到高位
-        /*   
-        // 首先翻转两个链表
-        ListNode prev = null;
-        ListNode curr = l1;
-        ListNode next = null;
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        l1 = prev;
-        
-        prev = null;
-        curr = l2;
-        next = null;
-        while(curr != null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-        l2 = prev;
-        */
+        return addTwoNumbersByTraverse(l1, l2);
+    }
 
-        //按位相加，记得进位
-        ListNode res = new ListNode(0);
-        int c = 0;
-        while (l1 != null && l2 != null) {
-            int currentL1 = l1.val;
-            int currentL2 = l2.val;
-            int sum = currentL1 + currentL2 + c;
-            c = 0;
-            if (sum > 9) {
-                c++;
-                sum = sum - 10;
-            }
-            ListNode last = res;
-            while (last.next != null) {
-                last = last.next;
-            }
-            last.next = new ListNode(sum);
+    /*
+    intuition:
+    like merge part of DnD
+    顺序遍历相加，注意最后有一个LL为null时，仍需要用另一个LL和carry继续运算,注意结束
+    https://leetcode.com/problems/add-two-numbers/discuss/1044/Java-concise-solution.
+
+    Say N is the length of LL1, M is the length of LL2
+    TC: O(Max(N, M)), N is the longer one of LL1 and LL2
+    SC: O(N + M)
+    */
+    private ListNode addTwoNumbersByTraverse(ListNode l1, ListNode l2){
+        // special case
+        if(l1 == null && l2 == null){
+            return null;
+        }
+        if(l1 == null){
+            return l2;
+        }
+        if(l2 == null){
+            return l1;
+        }
+
+        ListNode dummyRetHeadPre = new ListNode(-1); // dummy result List Head pre node, use to return ret node
+
+        ListNode curNode = dummyRetHeadPre; // traverse to build ret LL
+        int carry = 0; // carry
+        // loop to add
+        while(l1 != null && l2 != null){
+            // every time when we add, we have 3 digits, l1.val and l2.val and carry, we caculate their sum and then get the new carry to next loop and get the remainder as the new result list node.
+            int sum = l1.val + l2.val + carry; // current sum is l1.val + l2.val + carry
+
+            int remain = sum % 10; // cur remain
+            carry = sum / 10; // new carry
+
+            curNode.next = new ListNode(remain);
+
+            // move forward
             l1 = l1.next;
             l2 = l2.next;
+            curNode = curNode.next;
+        }
+        while(l1 != null){
+            int sum = l1.val + carry; // current sum is l1.val + carry
+
+            int remain = sum % 10; // cur remain
+            carry = sum / 10; // new carry
+
+            curNode.next = new ListNode(remain);
+
+            // move forward
+            l1 = l1.next;
+            curNode = curNode.next;
+        }
+        while(l2 != null){
+            int sum = l2.val + carry; // current sum is l1.val + carry
+
+            int remain = sum % 10; // cur remain
+            carry = sum / 10; // new carry
+
+            curNode.next = new ListNode(remain);
+
+            // move forward
+            l2 = l2.next;
+            curNode = curNode.next;
+        }
+        if(carry != 0){
+            curNode.next = new ListNode(carry);
         }
 
-        // 检查长链表
-        if (l1 != null) {
-
-            while (l1 != null) {
-                int sum = c + l1.val;
-                c = 0;
-                if (sum > 9) {
-                    c++;
-                    sum = sum - 10;
-                }
-                ListNode last = res;
-                while (last.next != null) {
-                    last = last.next;
-                }
-                last.next = new ListNode(sum);
-                l1 = l1.next;
-            }
-        }
-        if (l2 != null) {
-            while (l2 != null) {
-                int sum = c + l2.val;
-                c = 0;
-                if (sum > 9) {
-                    c++;
-                    sum = sum - 10;
-                }
-                ListNode last = res;
-                while (last.next != null) {
-                    last = last.next;
-                }
-                last.next = new ListNode(sum);
-                l2 = l2.next;
-            }
-        }
-
-        // 检查最后的进位位
-        if (c != 0) {
-            ListNode last = res;
-            while (last.next != null) {
-                last = last.next;
-            }
-            last.next = new ListNode(c);
-        }
-
-        //remove the first 0 node
-        return res.next;
+        // return the result list head
+        return dummyRetHeadPre.next;
     }
 
-    // 后续来自Leetcode： https://leetcode.com/problems/add-two-numbers/
-    // 思路是最直接的，包括自己的解法也是同样的思路，但是这个代码要简单的多:首先就是carry的求法用除法更好；然后不考虑额外求长链表的问题，而是一直遍历
-    // 完所有的链表，已经到末尾的直接视为val为0；最后，注意dummyHead的用法，比自己那个愚蠢的每次找最后一个非null节点正常多了。
-    // Time complexity : O(\max(m, n))O(max(m,n)). Assume that mm and nn represents the length of l1l1 and l2l2 respectively, 
-    // the algorithm above iterates at most \max(m, n)max(m,n) times.
-    public ListNode addTwoNumbersElementaryMath(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null)
-                p = p.next;
-            if (q != null)
-                q = q.next;
-        }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
-        }
-        return dummyHead.next;
-    }
 }
-
-// 最后，Leetcode提到了一个followup，是说如果输入链表不是对应数字的倒序，应该如何？
-// 没有提如何解决，自己感觉：那就先翻转，然后按照这个题目的方式来求，然后再翻转输出
