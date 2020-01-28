6d9417ac44f7150707a58ed67646bbb868b4568c
Kenshin <kenshinsyrup@gmail.com>
2020-01-27 16:29:01 -0800

    Add a TLE solution to MyCalendarI.

--- src/main/java/com/myleetcode/line_sweep/my_calendar_i/Solution.java
+++ src/main/java/com/myleetcode/line_sweep/my_calendar_i/Solution.java
@@ -11,22 +11,67 @@ class MyCalendar {
         calendarTreeMap = new TreeMap<Integer, Integer>();
     }
 
+    /*
+    Line Sweep with TreeMap:
+    Explaination:
+    https://leetcode.com/problems/my-calendar-i/discuss/109475/JavaC%2B%2B-Clean-Code-with-Explanation
+
+    N is total nodes in TreeMap.
+    TC: O(logN)
+    SC: O(1)
+    */
     public boolean book(int start, int end) {
+        return bookByCheckFirst(start, end);
+        // return simulateBookByTreeMap(start, end);
+    }
+
+    private boolean bookByCheckFirst(int start, int end) {
         // previous event's start time
         // floorKey() : It returns the greatest key less than or equal to the given key, or null if there is no such key.
         Integer prevStart = calendarTreeMap.floorKey(start);
 
         // next event's start time
-        // his method returns the least key which is greater than or equal to the given key value. If such a key is absent, null is returned.
+        // ceilingKey method returns the least key which is greater than or equal to the given key value. If such a key is absent, null is returned.
         Integer nextStart = calendarTreeMap.ceilingKey(start);
 
         // (prevStart not exist or prevStart's corresponding event's end time is less than start) AND (nextStart not exist or nextStart is greater than end), there's no overlap.
-        if((prevStart == null || calendarTreeMap.get(prevStart) <= start) && (nextStart == null || nextStart >= end)){// be careful with the two <=
+        if ((prevStart == null || calendarTreeMap.get(prevStart) <= start) && (nextStart == null || nextStart >= end)) {// be careful with the two <=
             calendarTreeMap.put(start, end);// must put
             return true;// true
         }
 
         return false;
+    }
+
+    /*
+    TLE
+
+    Template for My Calendar II: double book
+    https://leetcode.com/problems/my-calendar-ii/solution/
+
+    N is total nodes in TreeMap.
+    TC: O(N)
+    SC: O(1)
+    */
+    private boolean simulateBookByTreeMap(int start, int end) {
+        // Book.
+        this.calendarTreeMap.put(start, this.calendarTreeMap.getOrDefault(start, 0) + 1);
+        this.calendarTreeMap.put(end, this.calendarTreeMap.getOrDefault(end, 0) - 1);
+
+        // Check.
+        int meetingOnGoing = 0;
+        for (int time : this.calendarTreeMap.keySet()) {
+            meetingOnGoing += this.calendarTreeMap.get(time);
+
+            if (meetingOnGoing > 1) {
+                // Should not book, remove.
+                this.calendarTreeMap.put(start, this.calendarTreeMap.getOrDefault(start, 0) - 1);
+                this.calendarTreeMap.put(end, this.calendarTreeMap.getOrDefault(end, 0) + 1);
+                return false;
+            }
+        }
+
+        return true;
 
     }
 }
@@ -36,4 +81,3 @@ class MyCalendar {
  * MyCalendar obj = new MyCalendar();
  * boolean param_1 = obj.book(start,end);
  */
-
