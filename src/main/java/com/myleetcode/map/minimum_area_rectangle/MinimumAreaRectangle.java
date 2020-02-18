af800008ef9b827ad3af402b533f902b0780419b
Kenshin <kenshinsyrup@gmail.com>
2020-02-18 14:49:47 -0800

    HashTable: 939. Minimum Area Rectangle

---
+++ src/main/java/com/myleetcode/map/minimum_area_rectangle/MinimumAreaRectangle.java
@@ -0,0 +1,44 @@
+package com.myleetcode.map.minimum_area_rectangle;
+
+public class MinimumAreaRectangle {
+    /**
+     * 939. Minimum Area Rectangle
+     * Medium
+     *
+     * 479
+     *
+     * 92
+     *
+     * Add to List
+     *
+     * Share
+     * Given a set of points in the xy-plane, determine the minimum area of a rectangle formed from these points, with sides parallel to the x and y axes.
+     *
+     * If there isn't any rectangle, return 0.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: [[1,1],[1,3],[3,1],[3,3],[2,2]]
+     * Output: 4
+     * Example 2:
+     *
+     * Input: [[1,1],[1,3],[3,1],[3,3],[4,1],[4,3]]
+     * Output: 2
+     *
+     *
+     * Note:
+     *
+     * 1 <= points.length <= 500
+     * 0 <= points[i][0] <= 40000
+     * 0 <= points[i][1] <= 40000
+     * All points are distinct.
+     */
+
+    /**
+     * Hash Table
+     *
+     * Set
+     */
+}
