555af6ea79aab8cd522d13af2c365f3e6847721b
Kenshin <kenshinsyrup@gmail.com>
2020-04-13 15:38:46 -0700

    Priority Queue: 264. Ugly Number II

---
+++ src/main/java/com/myleetcode/priority_queue/ugly_number_ii/UglyNumberII.java
@@ -0,0 +1,35 @@
+package com.myleetcode.priority_queue.ugly_number_ii;
+
+public class UglyNumberII {
+    /**
+     * 264. Ugly Number II
+     * Medium
+     *
+     * 1406
+     *
+     * 87
+     *
+     * Add to List
+     *
+     * Share
+     * Write a program to find the n-th ugly number.
+     *
+     * Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+     *
+     * Example:
+     *
+     * Input: n = 10
+     * Output: 12
+     * Explanation: 1, 2, 3, 4, 5, 6, 8, 9, 10, 12 is the sequence of the first 10 ugly numbers.
+     * Note:
+     *
+     * 1 is typically treated as an ugly number.
+     * n does not exceed 1690.
+     */
+
+    /**
+     * Math
+     * DP
+     * Priority Queue
+     */
+}
