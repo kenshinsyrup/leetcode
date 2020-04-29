3ff67b9d48e1049ab95963056d1401a66876ef0f
Kenshin <kenshinsyrup@gmail.com>
2020-04-29 13:47:29 -0700

    Refactor: 560. Subarray Sum Equals K

--- src/main/java/com/myleetcode/presum/subarray_sum_equals_k/Solution.java
+++ src/main/java/com/myleetcode/presum/subarray_sum_equals_k/Solution.java
@@ -7,8 +7,41 @@ class Solution {
     public int subarraySum(int[] nums, int k) {
         // return subarraySumByPreSum(nums, k);
         // return subarraySumByPreSumII(nums, k);
-        // return subarraySubByPreSumAndMap(nums, k);
-        return subarraySumByPrefixSum(nums, k);
+        // return subarraySumByPrefixSum(nums, k);
+        return subarraySumByPresumAndMap(nums, k);
+    }
+
+    private int subarraySumByPresumAndMap(int[] nums, int K){
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int len = nums.length;
+        int count = 0;
+        int[] presums = new int[len + 1];
+        Map<Integer, Integer> presumNumMap = new HashMap<>();
+        for(int i = 0; i < len; i++){
+            // Get current presum.
+            presums[i + 1] = presums[i] + nums[i];
+
+            // For convience.
+            int presumIdx = i + 1;
+
+            // Check current presum.
+            if(presums[presumIdx] == K){
+                count++;
+            }
+            // Check target.
+            int target = presums[presumIdx] - K;
+            if(presumNumMap.containsKey(target)){
+                count += presumNumMap.get(target);
+            }
+
+            // Put for later use.
+            presumNumMap.put(presums[presumIdx], presumNumMap.getOrDefault(presums[presumIdx], 0) + 1);
+        }
+
+        return count;
     }
 
     /*
@@ -42,39 +75,6 @@ class Solution {
 
         return ret;
 
-    }
-
-    // 这个题还有一个优化，就是按照two sum那道题的样子，找余数。https://leetcode.com/problems/subarray-sum-equals-k/discuss/164431/Python-or-3-tm
-    // TC: O(N)
-    // SC: O(N)
-    // !!! a very important thing is, we could not first get all presums and then do another loop to find the target, this way may give us not subarray ans because when we are at idx i, we could not use the presum after i, but this way, we maybe used it.
-    // SO, MUST in one loop, first get presum and target, check target in current useable presums in presumNuMap and then put current presum to be used after
-    private int subarraySubByPreSumAndMap(int[] nums, int k) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-
-        int len = nums.length;
-        int[] presums = new int[len + 1];
-        Map<Integer, Integer> presumNumMap = new HashMap<>();
-        int count = 0;
-        for (int i = 0; i < presums.length; i++) {
-            // get presum
-            if (i >= 1) {
-                presums[i] = presums[i - 1] + nums[i - 1];
-            }
-
-            // check target in current existing presumNumMap
-            int target = presums[i] - k;
-            if (presumNumMap.containsKey(target)) {
-                count += presumNumMap.get(target);
-            }
-
-            // we have a new presum that we could use after
-            presumNumMap.put(presums[i], presumNumMap.getOrDefault(presums[i], 0) + 1);
-        }
-
-        return count;
     }
 
     // 这个有一个优化，就是我们不需要真的保存前项和到数组，可以用一个变量替代，这样节省额外的O(N)空间，最终可以达到O(1)空间
