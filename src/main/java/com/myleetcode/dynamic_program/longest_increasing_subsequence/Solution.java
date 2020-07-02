1bc4812c2e25d812ecd333c65583271b63c95fe8
Kenshin <kenshinsyrup@gmail.com>
2020-07-01 22:36:53 -0700

    DP, Refactor: 300. Longest Increasing Subsequence, 673. Number of Longest Increasing Subsequence

--- src/main/java/com/myleetcode/dynamic_program/longest_increasing_subsequence/Solution.java
+++ src/main/java/com/myleetcode/dynamic_program/longest_increasing_subsequence/Solution.java
@@ -1,59 +1,60 @@
 package com.myleetcode.dynamic_program.longest_increasing_subsequence;
 
-class Solution {
+public class Solution {
     public int lengthOfLIS(int[] nums) {
         return lenghtOfLISByDP(nums);
     }
 
     /*
-given sequence A[0: lenA-1]
+    Given sequence A[0: lenA-1]
 
-1 dp array为一维
+    1. dp array为一维
 
-2 dp[i]代表，给定A中的前i个elems即A[0:i-1], sequence最后一个elem为A[i-1]的情况下，LIS的长度
+    2. dp[i]代表，给定A中的前i个elems即A[0:i-1], sequence最后一个elem为A[i-1]的情况下，LIS的长度
 
-3
-dp[i] = max{
-  dp[i-1] + 1, if A[i-1] > A[i-2]
-  dp[i-2] + 1, if A[i-1] > A[i-3]
-  ...
-  dp[1] + 1, if A[i-1] > A[0]
-}
-最后取dp数组中最大值即为数组A的LIS
+    3.
+    dp[i] = max{
+      dp[i-1] + 1, if A[i-1] > A[i-2]
+      dp[i-2] + 1, if A[i-1] > A[i-3]
+      ...
+      dp[1] + 1, if A[i-1] > A[0]
+    }
+    最后取dp数组中最大值即为数组A的LIS
 
-4 base case, dp[i]中i取值范围为[0:lenA-1]: 对于任意 i!=0, dp[i]代表的LIS最少也是只包含了A[i-1]这一个elem，所以dp[i]=1; 对于i==0, dp[0]=0
+    4 Base case, dp[i]中i取值范围为[0:lenA]
+        对于i==0, dp[0]=0
+        对于任意 i!=0, dp[i]代表的LIS最少也是只包含了A[i-1]这一个elem，所以dp[i]=1
     */
     // TC: O(N^2)
     // SC: O(N)
-    private int lenghtOfLISByDP(int[] nums){
-        if(nums == null || nums.length == 0){
+    private int lenghtOfLISByDP(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
 
         int len = nums.length;
-        int[] dp = new int[len + 1]; // default is 0
+        int[] dp = new int[len + 1];
 
-        // base case, every solo char is a LIS.
-        for(int i = 1; i <= len; i++){
+        // Base case 1, because dp array default is all 0, so actually we don't need to set dp[0]=0 explicitly.
+        dp[0] = 0;
+        // Base case 2, every char itself is a LIS.
+        for (int i = 1; i <= len; i++) {
             dp[i] = 1;
         }
-        // dp[0] means LIS length of A[:0-1] which not exists, so dp[0] should be 0.Because dp default is 0, so dp[0] we dont need to set again
 
-        // so, next we need to caculate dp[i] for i[2:n]
-        for(int i = 1; i <= len; i++){
-            for(int j = 1; j < i; j++){
-                if(nums[i - 1] > nums[j - 1]){
+        int maxLen = 1;
+        // Normal case.
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j < i; j++) {
+                if (nums[i - 1] > nums[j - 1]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+
+            // Update the maxLen.
+            maxLen = Math.max(maxLen, dp[i]);
         }
 
-        int max = Integer.MIN_VALUE;
-        for(int v: dp){
-            max = Math.max(max, v);
-        }
-
-        return max;
-
+        return maxLen;
     }
 }
--- src/main/java/com/myleetcode/dynamic_program/number_of_longest_increasing_subsequence/Solution.java
+++ src/main/java/com/myleetcode/dynamic_program/number_of_longest_increasing_subsequence/Solution.java
@@ -1,6 +1,6 @@
 package com.myleetcode.dynamic_program.number_of_longest_increasing_subsequence;
 
-class Solution {
+public class Solution {
     public int findNumberOfLIS(int[] nums) {
         return findNumberOfLISByDP(nums);
     }
@@ -8,41 +8,45 @@ class Solution {
     // TC: O(N^2)
     // SC: O(N)
     // intuition: DP. 300, 673
-    // same as 300, but we need another dp array to help us with the counting.
-    private int findNumberOfLISByDP(int[] nums){
-        if(nums == null || nums.length == 0){
+    // same as 300, but we need another array count[i] to help us keep record of the LIS number of dp[i].
+    private int findNumberOfLISByDP(int[] nums) {
+        if (nums == null || nums.length == 0) {
             return 0;
         }
 
         int len = nums.length;
         int[] dp = new int[len + 1];
         int[] counts = new int[len + 1];
-        for(int i = 1; i <= len; i++){
+
+        // Base case.
+        dp[0] = 0;
+        for (int i = 1; i <= len; i++) {
             dp[i] = 1;
             counts[i] = 1;
         }
 
         int maxLen = 1;
-        // dp
-        for(int i = 1; i <= len; i++){
-            for(int j = 1; j < i; j++){
-                if(nums[i - 1] > nums[j - 1]){
-                    if(dp[i] < dp[j] + 1){
+        // Normal case.
+        for (int i = 1; i <= len; i++) {
+            for (int j = 1; j < i; j++) {
+                if (nums[i - 1] > nums[j - 1]) {
+                    if (dp[i] < dp[j] + 1) {
                         dp[i] = dp[j] + 1;
                         counts[i] = counts[j];
-                    }else if(dp[i] == dp[j] + 1){
+                    } else if (dp[i] == dp[j] + 1) {
                         counts[i] += counts[j];
                     }
                 }
             }
-            // get the max length
+
+            // Get the max length
             maxLen = Math.max(maxLen, dp[i]);
         }
 
         int ret = 0;
-        // counts the LIS num
-        for(int i = 1; i <= len; i++){
-            if(dp[i] == maxLen){
+        // Counts the LIS num
+        for (int i = 1; i <= len; i++) {
+            if (dp[i] == maxLen) {
                 ret += counts[i];
             }
         }
