d30c07bd9c616e24455148809c40aaffd437af57
Kenshin <kenshinsyrup@gmail.com>
2020-08-18 15:09:40 -0700

    BFS, Optimize: 127. Word Ladder, 126. Word Ladder II

--- src/main/java/com/myleetcode/breadth_first_search/word_ladder/Solution.java
+++ src/main/java/com/myleetcode/breadth_first_search/word_ladder/Solution.java
@@ -2,27 +2,26 @@ package com.myleetcode.breadth_first_search.word_ladder;
 
 import java.util.*;
 
-class Solution {
+public class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-//         最短路径问题用bfs
+        return shortestPathByBFS(wordList, beginWord, endWord);
+    }
 
-        // special case
-        if(beginWord == null || endWord == null || wordList == null || !wordList.contains(endWord)){
+    // TC: O(N + M * K), K is the word length
+    // SC: O(N)
+    private int shortestPathByBFS(List<String> wordList, String beginWord, String endWord) {
+        // Special case
+        if (beginWord == null || endWord == null || wordList == null || wordList.size() == 0) {
             return 0;
         }
 
-        // return shortestPathByBFS(wordList, beginWord, endWord);
-        return shortestPathByBFSII(wordList, beginWord, endWord);
-    }
-
-    // 写法标准化
-    // TC: O(N + M * K), K is the word length
-    // SC: O(N)
-    private int shortestPathByBFSII(List<String> wordList, String startStr, String endStr){
         // because no duplicates, transfer the wordList to wordSet, to reduce the search time
         Set<String> wordSet = new HashSet<>();
-        for(String wordStr: wordList){
+        for (String wordStr : wordList) {
             wordSet.add(wordStr);
+        }
+        if (!wordSet.contains(endWord)) {
+            return 0;
         }
 
         // visited
@@ -31,47 +30,33 @@ class Solution {
         // queue for bfs
         Deque<String> strQueue = new ArrayDeque<>();
         // enqueue the start string
-        strQueue.offer(startStr);
+        strQueue.offer(beginWord);
 
         // pathLen
         int pathLen = 0;
 
         // BFS
-        while(!strQueue.isEmpty()){
+        while (!strQueue.isEmpty()) {
             // level size
             int size = strQueue.size();
             // increase pathLen every level
             pathLen++;
 
-            for(int i = 0; i < size; i++){
+            for (int i = 0; i < size; i++) {
                 // cur str node
                 String curStr = strQueue.poll();
+                // add as visited
+                visitedStrSet.add(curStr);
 
                 // check if we find the end string
-                if(curStr.equals(endStr)){
+                if (curStr.equals(endWord)) {
                     return pathLen;
                 }
 
-                // stransfer to char[]
-                char[] curStrCharArray = curStr.toCharArray();
-
-                // try to find all possible next string
-                for(int j = 0; j < curStrCharArray.length; j++){
-                    for(char ch = 'a'; ch <= 'z'; ch++){
-                        char tempChar = curStrCharArray[j];
-                        curStrCharArray[j] = ch;
-
-                        // next possible string
-                        String nextPossibleStr = new String(curStrCharArray);
-                        if(wordSet.contains(nextPossibleStr) && !visitedStrSet.contains(nextPossibleStr)){
-                            // enqueue
-                            strQueue.offer(nextPossibleStr);
-                            // add as visited
-                            visitedStrSet.add(nextPossibleStr);
-                        }
-
-                        // restore char[]
-                        curStrCharArray[j] = tempChar;
+                List<String> children = getChildren(curStr, wordSet);
+                for (String child : children) {
+                    if (!visitedStrSet.contains(child)) {
+                        strQueue.offer(child);
                     }
                 }
             }
@@ -80,62 +65,28 @@ class Solution {
         return 0;
     }
 
+    // Find all children of given word.
+    private List<String> getChildren(String word, Set<String> wordSet) {
+        List<String> children = new ArrayList<>();
+        char chs[] = word.toCharArray();
 
-    private int shortestPathByBFS(List<String> wordList, String start, String end){
-        // use queue to bfs
-        Queue<String> stringQ = new LinkedList<String>();
-        // add first element to queue to start
-        stringQ.add(start);
+        for (int i = 0; i < chs.length; i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (chs[i] != ch) {
+                    // !!! keep the original char to restore because we should only chage one char in chs and we should try 'a' to 'z' so everytime we try, we restore and try next char in 'a' to 'z'
+                    char old_ch = chs[i];
+                    chs[i] = ch;
 
-        // need a set to store visited string
-        Set<String> visited = new HashSet<>();
-        // visited
-        visited.add(start);
-
-        // 真实层数，即path length + 1
-        // 这个问题最重要的一点，是level起始值是1而不是0，这个读题目就能看出来，最初因为不读题，导致算出来的level少了一。注意，这个题目要找的不是（不只是）从list的第一个元素到最后一个元素的长度，还需要包括一个长度就是从给定字符串到list的第一个元素这个长队，也就是bfs遍历了整个list之后如果能找到ladder需要让level+1，但是如果没有找到level需要是0.所以这个level就需要在最初的时候从1开始，如果没有找到，返回0，如果找到了，那么level从1开始++
-        int level = 1;
-        // int level = 0;
-        // level++;
-
-        // 控制bfs层数，用来记录何时是一层从而正确的让level计数
-        int lenControl = 0;
-//         初始层数
-        lenControl = stringQ.size();
-
-        while(!stringQ.isEmpty()){
-            String str = stringQ.poll();
-            if(str.equals(end)){
-                return level;
-            }
-
-            // lenControl减一，queue中该区段的node被取出了一个
-            lenControl--;
-
-            // change its character
-            for(int i = 0; i < str.length(); i++){
-                char[] charArray = str.toCharArray();
-
-                for(char c = 'a'; c <= 'z'; c++){
-                    charArray[i] = c;
-
-                    String newStr = new String(charArray);
-
-                    if(wordList.contains(newStr) && !visited.contains(newStr)){
-                        stringQ.add(newStr);
-                        visited.add(newStr);
+                    String child = new String(chs);
+                    if (wordSet.contains(child)) {
+                        children.add(child);
                     }
-                }
-            }
 
-//             根据lenControl是否为０，也就是根据queue中同一层的node是否都被取出作为一层结束的标记
-            if(lenControl == 0){
-                lenControl = stringQ.size();
-                level++;
+                    chs[i] = old_ch;
+                }
             }
         }
 
-        return 0;
-
+        return children;
     }
 }
--- src/main/java/com/myleetcode/breadth_first_search/word_ladder_ii/Solution.java
+++ src/main/java/com/myleetcode/breadth_first_search/word_ladder_ii/Solution.java
@@ -4,112 +4,122 @@ import java.util.*;
 
 public class Solution {
     public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
-        // return findLaddersByDFS(beginWord, endWord, wordList);// Wrong, TLE
-        return findLaddersByBFSAndDFS(beginWord, endWord, wordList);
+        // return findLaddersByDFS(beginWord, endWord, wordList);// Naive Backtracking, TLE
+        return findLaddersByBFSAndBacktracking(beginWord, endWord, wordList);
     }
 
-    // the proble needs us to return All Shortest Paths
-    // so the correct thought is: 1 we use BFS to find the shortest path from beginWord to endWord and during this we could build a Directional Graph using a Map<String, List<String>>. 2 we use DFS to find all paths in this DIrectional Graph from beginWord to endWord
+    /*
+    The proble needs us to return All Shortest Paths. As we know we could use BFS from beginWord to get endWord.
+
+    About how to get shortest path in a BFS:
+    Based on the BFS, if we want to know the exact paths we traveled, we could record the parents of each word, and distance from each word to beginWord during the BFS. And then using Backtracking with these two maps to recover all the shortest paths.
+
     // thought: https://leetcode.com/problems/word-ladder-ii/discuss/172308/A-kind-of-concept-with-diagrams-()
     // solution: https://leetcode.com/problems/word-ladder-ii/discuss/40475/My-concise-JAVA-solution-based-on-BFS-and-DFS
-    private List<List<String>> findLaddersByBFSAndDFS(String beginWord, String endWord, List<String> wordList){
-        List<List<String>> ret = new ArrayList<>();
 
-        if(beginWord == null || beginWord.length() == 0 || endWord == null || endWord.length() == 0 || wordList == null || wordList.size() == 0){
+    for eg, input:
+        beginWord: "a"
+        endWord: "c"
+        wordList: ["a","b","c"]
+
+    will give us two map after BFS:
+        wordParentMap: {a=[b, c], b=[a, c], c=[a, b]}
+        wordDistanceMap: {a=0, b=1, c=1}
+
+    we could recover the path from endWord to beginWord based on these two maps:
+        c -> a
+        b -> a
+
+    after reverse, these will be what we want.
+    */
+    private List<List<String>> findLaddersByBFSAndBacktracking(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> ret = new ArrayList<>();
+        if (beginWord == null || beginWord.length() == 0 || endWord == null || endWord.length() == 0 || wordList == null || wordList.size() == 0) {
             return ret;
         }
 
         Set<String> wordSet = new HashSet<>(wordList);
-        Map<String, List<String>> graph = new HashMap<>();// directional graph for every node
-        Map<String, Integer> wordDistanceMap = new HashMap<>();// Distance of every node from the start node
+
+        // <word, its parent word>, keep tracking of the parents of each word during BFS.
+        Map<String, List<String>> wordParentMap = new HashMap<>();
+        // <word, distance to beginWord>, keep tracking of distance from word to the beginWord during BFS.
+        Map<String, Integer> wordDistanceMap = new HashMap<>();
+
+        // Search shortest path from beginWord to endWord, and keep the information of parents and distanct to beginWord for each word during BFS.
+        Set<String> visited = new HashSet<>();
+        BFS(beginWord, endWord, wordSet, wordParentMap, wordDistanceMap);
+
+        // Using Backtracking to recover all the shortest paths from endWord to beginWord.
         List<String> snippet = new ArrayList<>();
-
-        // add the given beginWord to the word Set
-        wordSet.add(beginWord);
-
-        // build the graph with the shortest path from beginWord to endWord
-        bfs(beginWord, endWord, wordSet, graph, wordDistanceMap);
-
-        // get all paths in the graph
-        snippet.add(beginWord);// according to the problem, the path should contains the beginWord
-        dfs(beginWord, endWord, wordSet, graph, wordDistanceMap, snippet, ret);
+        snippet.add(endWord);
+        backtrackingII(endWord, beginWord, wordParentMap, wordDistanceMap, snippet, ret);
 
         return ret;
     }
 
     // BFS: Trace every node's distance from the start node (level by level).
-    private void bfs(String beginWord, String endWord, Set<String> wordSet, Map<String, List<String>> graph, Map<String, Integer> wordDistanceMap) {
-
-        for (String str : wordSet){
-            graph.put(str, new ArrayList<String>());
-        }
-
+    private void BFS(String beginWord, String endWord, Set<String> wordSet, Map<String, List<String>> wordParentMap, Map<String, Integer> wordDistanceMap) {
         Queue<String> queue = new LinkedList<String>();
         queue.offer(beginWord);
         wordDistanceMap.put(beginWord, 0);
+        // wordParentMap.put(beginWord, new ArrayList<>());
 
         while (!queue.isEmpty()) {
-            int count = queue.size();
-            boolean foundEnd = false;
+            String cur = queue.poll();
 
-            for (int i = 0; i < count; i++) {
-                String cur = queue.poll();
-                int curDistance = wordDistanceMap.get(cur);
-                List<String> neighbors = getNeighbors(cur, wordSet);
+            int curDistance = wordDistanceMap.get(cur);
+            List<String> children = getChildren(cur, wordSet);
+            for (String child : children) {
+                // Add cur word as child's parent.
+                List<String> parents = wordParentMap.getOrDefault(child, new ArrayList<>());
+                parents.add(cur);
+                wordParentMap.put(child, parents);
 
-                for (String neighbor : neighbors) {
-                    graph.get(cur).add(neighbor);
+                // If we don't know the distance from child to beginWord, means this is the first time we meet this word and this is the shortest distance to reach child. So we keep record of this distance, and enque this child word.
+                if (!wordDistanceMap.containsKey(child)) {
+                    wordDistanceMap.put(child, curDistance + 1);
 
-                    if (!wordDistanceMap.containsKey(neighbor)) {// Check if visited
-                        wordDistanceMap.put(neighbor, curDistance + 1);
-
-                        if (endWord.equals(neighbor)){
-                            // Found the shortest path
-                            foundEnd = true;
-                        }else{
-                            queue.offer(neighbor);
-                        }
-                    }
-                }
-
-                if (foundEnd){
-                    break;
+                    queue.offer(child);
                 }
             }
         }
     }
 
-    // DFS: output all paths with the shortest distance.
-    private void dfs(String curWord, String endWord, Set<String> wordSet, Map<String, List<String>> graph, Map<String, Integer> wordDistanceMap, List<String> snippet, List<List<String>> ret) {
-        if (endWord.equals(curWord)) {
-            ret.add(new ArrayList<String>(snippet));
+    // Backtracking: output all shortest paths.
+    private void backtrackingII(String curWord, String beginWord, Map<String, List<String>> wordParentMap, Map<String, Integer> wordDistanceMap, List<String> snippet, List<List<String>> ret) {
+        if (curWord.equals(beginWord)) {
+            // !!! Don't reverse snippet directly, in backtracking, the order means a lot.
+            List<String> path = new ArrayList<>(snippet);
+            Collections.reverse(path);
+            ret.add(path);
             return;
         }
 
-        for (String nextWord: graph.get(curWord)) {
-            if (wordDistanceMap.get(nextWord) == wordDistanceMap.get(curWord) + 1) {
-                snippet.add(nextWord);
-                dfs(nextWord, endWord, wordSet, graph, wordDistanceMap, snippet, ret);
+        List<String> parents = wordParentMap.getOrDefault(curWord, new ArrayList<>());
+        for (String parent : parents) {
+            if (wordDistanceMap.get(parent) == wordDistanceMap.get(curWord) - 1) { // !!!
+                snippet.add(parent);
+                backtrackingII(parent, beginWord, wordParentMap, wordDistanceMap, snippet, ret);
                 snippet.remove(snippet.size() - 1);
             }
         }
     }
 
-    // Find all neighbors of given word   
-    private List<String> getNeighbors(String word, Set<String> wordSet) {
-        List<String> neighbors = new ArrayList<>();
+    // Find all children of given word.
+    private List<String> getChildren(String word, Set<String> wordSet) {
+        List<String> children = new ArrayList<>();
         char chs[] = word.toCharArray();
 
         for (int i = 0; i < chs.length; i++) {
-            for (char ch ='a'; ch <= 'z'; ch++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
                 if (chs[i] != ch) {
                     // !!! keep the original char to restore because we should only chage one char in chs and we should try 'a' to 'z' so everytime we try, we restore and try next char in 'a' to 'z'
                     char old_ch = chs[i];
                     chs[i] = ch;
 
-                    String neighborWord = new String(chs);
-                    if (wordSet.contains(neighborWord)) {
-                        neighbors.add(neighborWord);
+                    String child = new String(chs);
+                    if (wordSet.contains(child)) {
+                        children.add(child);
                     }
 
                     chs[i] = old_ch;
@@ -117,9 +127,8 @@ public class Solution {
             }
         }
 
-        return neighbors;
+        return children;
     }
-
 
     // intuition: this is different with the 127. Word Ladder. The 127 asks fot the shortest path so we use BFS. This problem asks for all paths, so we need DFS.
     // the thought is we use DFS Backtracking to tray all paths from beginWord, if one path reaches the endWord successfully, we could record it tho the List ret.
@@ -128,15 +137,15 @@ public class Solution {
     // Naive Backtracking: TLE
     // TC: O(2^N), N is the length of wordList, is the length of recursion
     // SC: O(N)
-    private List<List<String>> findLaddersByDFS(String beginWord, String endWord, List<String> wordList){
+    private List<List<String>> findLaddersByDFS(String beginWord, String endWord, List<String> wordList) {
         List<List<String>> ret = new ArrayList<>();
 
-        if(beginWord == null || beginWord.length() == 0 || endWord == null || endWord.length() == 0 || wordList == null || wordList.size() == 0){
+        if (beginWord == null || beginWord.length() == 0 || endWord == null || endWord.length() == 0 || wordList == null || wordList.size() == 0) {
             return ret;
         }
 
         Map<String, Boolean> wordVisitMap = new HashMap<>();
-        for(String word: wordList){
+        for (String word : wordList) {
             wordVisitMap.put(word, false);
         }
 
@@ -147,13 +156,13 @@ public class Solution {
 
         // choose the shortest paths to return
         int shortest = Integer.MAX_VALUE;
-        for(List<String> path: ret){
+        for (List<String> path : ret) {
             shortest = Math.min(shortest, path.size());
         }
 
         List<List<String>> shortestPath = new ArrayList<>();
-        for(List<String> path: ret){
-            if(path.size() == shortest){
+        for (List<String> path : ret) {
+            if (path.size() == shortest) {
                 path.add(0, beginWord);// must add the real start word into the path according the problem
                 shortestPath.add(path);
             }
@@ -163,27 +172,27 @@ public class Solution {
     }
 
     // backtracking to try to find all possible path
-    private void backtracking(String beginWord, String endWord, Map<String, Boolean>wordVisitMap, List<String> snippet, List<List<String>> ret){
-        if(beginWord.equals(endWord)){
+    private void backtracking(String beginWord, String endWord, Map<String, Boolean> wordVisitMap, List<String> snippet, List<List<String>> ret) {
+        if (beginWord.equals(endWord)) {
             ret.add(new ArrayList<>(snippet));
             return;
         }
 
         wordVisitMap.put(beginWord, true);
 
-        for(int i = 0; i < beginWord.length(); i++){
+        for (int i = 0; i < beginWord.length(); i++) {
             char[] charArray = beginWord.toCharArray();
 
-            for(char c = 'a'; c <= 'z'; c++){
+            for (char c = 'a'; c <= 'z'; c++) {
                 charArray[i] = c;
                 String nextStr = new String(charArray);
 
-                if(wordVisitMap.containsKey(nextStr) && !wordVisitMap.get(nextStr)){
+                if (wordVisitMap.containsKey(nextStr) && !wordVisitMap.get(nextStr)) {
                     snippet.add(nextStr);
 
                     backtracking(nextStr, endWord, wordVisitMap, snippet, ret);
 
-                    snippet.remove(snippet.size() -1);
+                    snippet.remove(snippet.size() - 1);
                 }
             }
         }
