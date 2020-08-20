29c2c3a82a65f5ef8ef9203b6abdfacc73738d59
Kenshin <kenshinsyrup@gmail.com>
2020-08-20 14:50:42 -0700

    TreeMap, Optimize: 729. My Calendar I

--- src/main/java/com/myleetcode/line_sweep/my_calendar_i/Solution.java
+++ src/main/java/com/myleetcode/line_sweep/my_calendar_i/Solution.java
@@ -4,39 +4,34 @@ import java.util.TreeMap;
 
 class MyCalendar {
     // private attribute. First we need a map, key is start time and value is end time. Then we need a TreeMap, so we could find the keys near a given 'key' but we don't know if this given 'key' exists in the map.
-    private TreeMap<Integer, Integer> calendarTreeMap;
+    private TreeMap<Integer, Integer> calendarTM;
 
     public MyCalendar() {
-        // init
-        calendarTreeMap = new TreeMap<Integer, Integer>();
+        this.calendarTM = new TreeMap<>();
     }
 
     /*
-    Line Sweep with TreeMap:
-    Explaination:
-    https://leetcode.com/problems/my-calendar-i/discuss/109475/JavaC%2B%2B-Clean-Code-with-Explanation
-
     N is total nodes in TreeMap.
     TC: O(logN)
     SC: O(1)
     */
     public boolean book(int start, int end) {
-        return bookByCheckFirst(start, end);
-        // return simulateBookByTreeMap(start, end);
+        // return bookByCheckFirst(start, end); // Much faster.
+        return simulateBookByTreeMap(start, end); // Template for My Calendar II: double book.
     }
 
     private boolean bookByCheckFirst(int start, int end) {
         // previous event's start time
         // floorKey() : It returns the greatest key less than or equal to the given key, or null if there is no such key.
-        Integer prevStart = calendarTreeMap.floorKey(start);
+        Integer prevStart = calendarTM.floorKey(start);
 
         // next event's start time
-        // ceilingKey method returns the least key which is greater than or equal to the given key value. If such a key is absent, null is returned.
-        Integer nextStart = calendarTreeMap.ceilingKey(start);
+        // his method returns the least key which is greater than or equal to the given key value. If such a key is absent, null is returned.
+        Integer nextStart = calendarTM.ceilingKey(start);
 
         // (prevStart not exist or prevStart's corresponding event's end time is less than start) AND (nextStart not exist or nextStart is greater than end), there's no overlap.
-        if ((prevStart == null || calendarTreeMap.get(prevStart) <= start) && (nextStart == null || nextStart >= end)) {// be careful with the two <=
-            calendarTreeMap.put(start, end);// must put
+        if ((prevStart == null || calendarTM.get(prevStart) <= start) && (nextStart == null || nextStart >= end)) {// be careful with the two <=
+            calendarTM.put(start, end);// must put
             return true;// true
         }
 
@@ -44,8 +39,6 @@ class MyCalendar {
     }
 
     /*
-    TLE
-
     Template for My Calendar II: double book
     https://leetcode.com/problems/my-calendar-ii/solution/
 
@@ -55,24 +48,23 @@ class MyCalendar {
     */
     private boolean simulateBookByTreeMap(int start, int end) {
         // Book.
-        this.calendarTreeMap.put(start, this.calendarTreeMap.getOrDefault(start, 0) + 1);
-        this.calendarTreeMap.put(end, this.calendarTreeMap.getOrDefault(end, 0) - 1);
+        this.calendarTM.put(start, this.calendarTM.getOrDefault(start, 0) + 1);
+        this.calendarTM.put(end, this.calendarTM.getOrDefault(end, 0) - 1);
 
         // Check.
         int meetingOnGoing = 0;
-        for (int time : this.calendarTreeMap.keySet()) {
-            meetingOnGoing += this.calendarTreeMap.get(time);
+        for (int time : this.calendarTM.keySet()) {
+            meetingOnGoing += this.calendarTM.get(time);
 
             if (meetingOnGoing > 1) {
                 // Should not book, remove.
-                this.calendarTreeMap.put(start, this.calendarTreeMap.getOrDefault(start, 0) - 1);
-                this.calendarTreeMap.put(end, this.calendarTreeMap.getOrDefault(end, 0) + 1);
+                this.calendarTM.put(start, this.calendarTM.get(start) - 1);
+                this.calendarTM.put(end, this.calendarTM.get(end) + 1);
                 return false;
             }
         }
 
         return true;
-
     }
 }
 
